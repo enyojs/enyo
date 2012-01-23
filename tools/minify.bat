ee8f8364b117b2c49fe3128a09353b3a521fade2
Scott Miles <scott.miles@palm.com>
2012-01-23 11:51:44 -0800

    use REM not // to commment in batch files!

--- tools/minify.bat
+++ tools/minify.bat
@@ -1,17 +1,17 @@
-// don't watch the sausage being made
+REM don't watch the sausage being made
 @ECHO OFF
 
-// the folder this script is in (*/enyo/tools)
+REM the folder this script is in (*/enyo/tools)
 SET TOOLS=%~DP0
-// enyo location
+REM enyo location
 SET ENYO=%TOOLS%\..
-// default node location
+REM default node location
 SET NODE=%TOOLS%\node.exe
-// minify script location
+REM minify script location
 SET MINIFY=%TOOLS%\minifier\minify.js
 
-// use node to invoke minify with a known path to enyo and imported parameters
+REM use node to invoke minify with a known path to enyo and imported parameters
 %NODE% %MINIFY% -enyo %ENYO% %1 %2 %3 %4 %5 %6
 
-// let the user see the console output
+REM let the user see the console output
 PAUSE
