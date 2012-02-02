4423549ac3afcf5b7cc9219f68bbce2ba6357a5a
Scott Miles <scott.miles@palm.com>
2012-02-02 09:44:29 -0800

    remove dependency on local node.exe

--- tools/minify.bat
+++ tools/minify.bat
@@ -1,14 +1,17 @@
-REM don't watch the sausage being made
 @ECHO OFF
+REM don't watch the sausage being made
 
 REM the folder this script is in (*/enyo/tools)
 SET TOOLS=%~DP0
+
 REM enyo location
 SET ENYO=%TOOLS%\..
-REM default node location
-SET NODE=%TOOLS%\node.exe
+
 REM minify script location
 SET MINIFY=%TOOLS%\minifier\minify.js
+
+REM node location
+SET NODE=node.exe
 
 REM use node to invoke minify with a known path to enyo and imported parameters
 %NODE% %MINIFY% -enyo %ENYO% %1 %2 %3 %4 %5 %6
