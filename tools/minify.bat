1d246b71318c66bf93ce6aec3cfd11cb995fa356
Scott Miles <scott.miles@palm.com>
2012-02-02 19:30:45 -0800

    whitespace

--- tools/minify.bat
+++ tools/minify.bat
@@ -3,10 +3,13 @@ REM don't watch the sausage being made
 
 REM the folder this script is in (*/enyo/tools)
 SET TOOLS=%~DP0
+
 REM enyo location
 SET ENYO=%TOOLS%\..
+
 REM default node location
 SET NODE=%TOOLS%\node.exe
+
 REM minify script location
 SET MINIFY=%TOOLS%\minifier\minify.js
 
