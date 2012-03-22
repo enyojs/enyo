7ccf2f1168fe8e625d93895358396682e9acbe73
Steve Orvell <steve.orvell@palm.com>
2012-03-22 16:50:06 -0700

    * add onpostresize event.

--- source/kernel/UiComponent.js
+++ source/kernel/UiComponent.js
@@ -189,6 +189,7 @@ enyo.kind({
 	// syntactic sugar for 'waterfall("onresize")'
 	resized: function() {
 		this.waterfall("onresize");
+		this.waterfall("onpostresize");
 	},
 	//* @protected
 	resizeHandler: function() {
@@ -239,6 +240,7 @@ enyo.master = new enyo.Component({
 			// this works because master is a Component, so it' waterfalls
 			// to it's owned Components (i.e. master has no children)
 			enyo.master.waterfallDown("onresize");
+			enyo.master.waterfallDown("onpostresize");
 		} else {
 			// all other top level events are sent only to interested Signal receivers
 			enyo.Signals.send(inEventName, inEvent);
