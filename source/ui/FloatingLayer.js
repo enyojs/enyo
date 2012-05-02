fc89a9b6db982b947f43d91cf14b8940a3a372af
unwiredben <combee@techwood.org>
2012-05-02 00:36:03 -0500

    Fix mention of onyx in comments in FloatingLayer.js to enyo

--- source/ui/FloatingLayer.js
+++ source/ui/FloatingLayer.js
@@ -1,12 +1,13 @@
 /**
-	A control which provides a layer for controls which should be displayed above an application. The onyx.floatingLayer singleton
-	can be set as a control parent to have the control float above an application. 
+	A control which provides a layer for controls which should be displayed above an application. 
+	The enyo.floatingLayer singleton can be set as a control parent to have the control float 
+	above an application. 
 
-	Note: it's not intended that users create an onyx.FloatingLayer.
+	Note: it's not intended that users create an enyo.FloatingLayer.
 
 		create: function() {
 			this.inherited(arguments);
-			this.setParent(onyx.floatingLayer);
+			this.setParent(enyo.floatingLayer);
 		}
 
 */
