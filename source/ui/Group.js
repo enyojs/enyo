e2f26b2404addd34462664f60dbac7b7e99f249a
Daniel Freedman <dfreedm2@gmail.com>
2012-05-10 15:25:53 -0700

    Revert previous change, intention is to use <control>.setActive(true), and never <Group>.setActive(<control>)

--- source/ui/Group.js
+++ source/ui/Group.js
@@ -29,13 +29,15 @@ enyo.kind({
 			}
 		}
 	},
+	// XXX: The indended functionality is to use <component>.setActive(true), not Group.setActive(<component>)
+	setActive: function() {
+	},
 	activeChanged: function(inOld) {
 		if (inOld) {
 			inOld.setActive(false);
 			inOld.removeClass("active");
 		}
 		if (this.active) {
-			this.active.setActive(true);
 			this.active.addClass("active");
 		}
 	}
