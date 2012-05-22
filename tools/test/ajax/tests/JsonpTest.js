c6de3427df162f2e822c57bc748105584b9740f8
unwiredben <combee@techwood.org>
2012-05-22 13:42:02 -0500

    * enyo.JsonpRequest: add charset parameter and cacheBust parameters, error handling * Refactor script adding code, moving it out of the statics section * Add enyo.JsonpRequest tests

---
+++ tools/test/ajax/tests/JsonpTest.js
@@ -0,0 +1,29 @@
+enyo.kind({
+	name: "JsonpTest",
+	kind: enyo.TestSuite,
+	_testJsonp: function(inProps, inParams, inAssertFn) {
+		return new enyo.JsonpRequest(inProps)
+			.response(this, function(inSender, inValue) {
+				this.finish(inAssertFn.call(null, inValue) ? "" : "bad response: " + inValue);
+			})
+			.error(this, function(inSender, inValue) {
+				this.finish("bad status: " + inValue);
+				console.error(inValue);
+			})
+			.go(inParams);
+	},
+	_testResponse: function(inProps, inAssertFn) {
+		this._testJsonp(enyo.mixin({url: "php/test1.php?format=jsonp", callback: "callback"}, inProps), 
+			null, inAssertFn);
+	},
+	testJsonResponse: function() {
+		this._testResponse({}, function(inValue) {
+			return inValue.response == "hello";
+		});
+	},
+	testCharset: function() {
+		this._testResponse({charset: "utf8"}, function(inValue) {
+			return inValue.utf8 == "\u0412\u0438\u0301\u0445\u0440\u0438";
+		});
+	}
+});
