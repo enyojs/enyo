25309ffeda9d526a0601b9abc26ea3938ceebd65
unwiredben <combee@techwood.org>
2012-06-21 18:00:39 -0500

    Add note about enyo.Select not working for webOS apps

--- source/ui/Select.js
+++ source/ui/Select.js
@@ -17,6 +17,10 @@
 				this.sortListAscending();
 			}
 		}
+
+	Note: this uses the `<select>` tag which isn't implemented
+	for native webOS applications, although it does work in the
+	webOS web browser.
 */
 
 enyo.kind({
