d728ee4e31dc5f01a99c0ae205972a7953f66149
unwiredben <combee@techwood.org>
2012-06-26 23:28:54 -0700

    Add docs about oninput/onchange DOM events sent by enyo.Input

--- source/ui/Input.js
+++ source/ui/Input.js
@@ -1,6 +1,10 @@
 /**
 	Implements an HTML &lt;input&gt; element with cross-platform support for
 	change events.
+
+	You can listen for _oninput_ and _onchange_ DOM events from this control
+	to know when the text inside has been modified. _oninput_ fires immediately
+	while _onchange_ fires when the text has changed and the input loses focus.
 */
 enyo.kind({
 	name: "enyo.Input",
