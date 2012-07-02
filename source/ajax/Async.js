7d6c423b8e0878b65a77a30644313070df57a812
Jim Tang <jdtang@cww.org>
2012-07-02 14:10:56 -0700

    Moved some doc content to Async doc in Developer Guide

--- source/ajax/Async.js
+++ source/ajax/Async.js
@@ -1,72 +1,21 @@
 ﻿/**
-	Base kind for handling asynchronous operations.
+	_enyo.Async_ is the base kind for handling asynchronous operations.
 	
-	_enyo.Aync_ is an **Object**, not a **Component**, you may not declare
-	them in component blocks. If you want to use Async as a Component, you
-	are probably looking for <a href="#enyo.WebService">enyo.WebService</a>.
+	_enyo.Async_ is an **Object**, not a **Component**; thus, you may not declare
+	an _Async_ in a _components_ block. If you want to use _Async_ as a
+	component, you should probably be using
+	<a href="#enyo.WebService">enyo.WebService</a> instead.
 	
-	An Async object represents a task that has not yet completed. Callback
-	functions can be attached to an Async that will be called with the task
-	completes or encounters an error.
+	An Async object represents a task that has not yet completed. You may attach
+	callback functions to an Async, to be called when the task completes or
+	encounters an error.
 	
-	To use it, create a new instance of _enyo.Async_ or a kind derived from it,
-	then register handlers with the `response()` and `error()` methods.
+	More information on _Async_ and its usage is available in the
+	<a href="https://github.com/enyojs/enyo/wiki/Async">Async documentation</a>
+	in the Enyo Developer Guide.
 	
-	Start the async operation by calling the `go()` method.
 
-	Handlers may either be methods with the signature _(asyncObject, value)_ or
-	new instances of _enyo.Async_ or its subkinds. This allows for chaining of
-	async objects (e.g., when calling a Web API).
 
-	If a response method returns a value (other than undefined) that value is 
-	sent to subsequent handlers in the chain, replacing the original value.
-
-	A failure method can call `recover()` to undo the error condition and switch
-	to calling response methods.
-
-	The default implementation of `go()` causes all the response handlers 
-	to be called (asynchronously).
-
-	Here is a complicated example which demonstrates many of these features:
-
-		var transaction = function() {
-			// create a transaction object
-			var async = new enyo.Async();
-			// cause handlers to fire asynchronously (sometime after we yield this thread)
-			// "initial response" will be sent to handlers as inResponse
-			async.go("intial response");
-			// until we yield the thread, we can continue to add handlers
-			async.response(function(inSender, inResponse) {
-				console.log("first response: returning a string, subsequent handlers receive this value for 'inResponse'");
-				return "some response"
-			});
-			return async;
-		};
-
-	Users of the `transaction()` function can add handlers to the Async object until all functions return (synchronously).
-
-		// get a new transaction, it's been started, but we can add more handlers synchronously
-		var x = transaction();
-
-		// add an handler that will be called if an error is detected ... this handler recovers and sends a custom message
-		x.error(function(inSender, inResponse) {
-			console.log("error: calling recover", inResponse);
-			this.recover();
-			return "recovered message";
-		});
-
-		// add a response handler that halts response handler and triggers the error chain
-		// the error will be sent to the error handler registered above, which will
-		// restart the handler chain
-		x.response(function(inSender, inResponse) {
-			console.log("response: calling fail");
-			this.fail(inResponse);
-		});
-
-		// recovered message will end up here
-		x.response(function(inSender, inResponse) {
-			console.log("response: ", inResponse);
-		});
 */
 enyo.kind({
 	name: "enyo.Async",
