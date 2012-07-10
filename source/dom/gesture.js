e2dd576deacf0718f9111e765fa708d80c9d7c92
Jim Tang <jdtang@cww.org>
2012-07-10 10:25:13 -0700

    Moved some doc content to Developer Guide

--- source/dom/gesture.js
+++ source/dom/gesture.js
@@ -4,46 +4,11 @@
 	all supported platforms. These events are provided so that users can write a
 	single set of event handlers for applications that run on both mobile and 
 	desktop platforms.  They are needed because desktop and mobile platforms
-	handle basic gestures differently.  For example, desktop platforms provide
-	mouse events, while mobile platforms support touch events and a limited set
-	of mouse events for backward compatibility.
-
-	The following events are available:
-
-	* "down" is generated when the pointer is pressed down.
-
-	* "up" is generated when the pointer is released up.
-
-	* "tap" is generated when the pointer is pressed down and released up.  The
-		target is the lowest DOM element that received both	the related "down"
-		and	"up" events.
-
-	* "move" is generated when the pointer moves.
-
-	* "enter" is generated when the pointer enters a DOM node.
-
-	* "leave" is generated when the pointer leaves a DOM node.
-
-	These events are synthesized from the available DOM events and contain these
-	common properties, when available:
-
-	* target
-	* relatedTarget
-	* clientX
-	* clientY
-	* pageX
-	* pageY
-	* screenX
-	* screenY
-	* altKey
-	* ctrlKey
-	* metaKey
-	* shiftKey
-	* detail
-	* identifier
-
-	Please note that Enyo's gesture events are generated on Enyo controls, not
-	DOM elements.
+	handle basic gestures differently.
+	
+	For more information on gesture events and their associated properties,	see
+	the documentation on [Gestures](https://github.com/enyojs/enyo/wiki/Gestures)
+	in the Enyo Developer Guide.
 */
 enyo.gesture = {
 	//* @protected
