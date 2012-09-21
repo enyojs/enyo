d5fd014a983e31978a87c8e5923a302bac55849e
Jim Tang <jim.tang@palm.com>
2012-09-21 15:14:21 -0700

    Adding link to 'Scrollers' doc in Dev Guide. Enyo-DCO-1.0-Signed-off-by: Jim Tang <jim.tang@palm.com>

--- source/touch/Scroller.js
+++ source/touch/Scroller.js
@@ -7,6 +7,10 @@ DOM elements.  In such cases, _enyo.Scroller_ implements a touch-based scrolling
 solution, which may be opted into either globally (by setting the flag
 _enyo.Scroller.touchScrolling = true;_) or on a per-instance basis (by
 specifying a _strategyKind_ of "TouchScrollStrategy").
+
+For more information, see the documentation on
+[Scrollers](https://github.com/enyojs/enyo/wiki/Scrollers) in the Enyo Developer
+Guide.
 */
 enyo.kind({
 	name: "enyo.Scroller",
