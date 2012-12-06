a5df66908b377b19753ea28801d01f412e445d0a
Ben Combee <ben.combee@palm.com>
2012-12-06 15:58:28 -0800

    ENYO-1609: gate calls to rendered() by checking to see if node was generated first
    Enyo-DCO-1.1-Signed-Off-By: Ben Combee (ben.combee@palm.com)

--- source/dom/Control.js
+++ source/dom/Control.js
@@ -151,10 +151,10 @@ enyo.kind({
 		Returns the DOM node representing the control.
 		If the control is not currently rendered, returns null.
 		
-		If hasNode() returns a value, the _node_ property will be valid and 
+		If hasNode() returns a value, the _node_ property will be valid and
 		can be checked directly.
 		
-		Once hasNode() is called, the returned value is made available in 
+		Once hasNode() is called, the returned value is made available in
 		the _node_ property of this control.
 
 		A control will only return a node if it has been rendered.
@@ -232,7 +232,7 @@ enyo.kind({
 		}
 	},
 	/**
-		Convenience function for setting the _class_ attribute. 
+		Convenience function for setting the _class_ attribute.
 		The _class_ attribute represents the CSS classes assigned to this object;
 		it is a string that can contain multiple CSS classes separated by spaces.
 
@@ -242,7 +242,7 @@ enyo.kind({
 		this.setAttribute("class", inClass);
 	},
 	/**
-		Convenience function for getting the _class_ attribute. 
+		Convenience function for getting the _class_ attribute.
 		The _class_ attribute represents the CSS classes assigned to this object;
 		it is a string that can contain multiple CSS classes separated by spaces.
 
@@ -324,9 +324,9 @@ enyo.kind({
 		// We may need a 'runtimeStyles' concept separate from a 'userStyles' concept, although
 		// it's not clear what API calls like 'applyStyle' would affect, and which concept would take
 		// precedence when there is a conflict.
-		// Perhaps we can separate 'style' completely from 'domStyles'. API methods like applyStyle 
+		// Perhaps we can separate 'style' completely from 'domStyles'. API methods like applyStyle
 		// would affect domStyles, and the two style databases would be combined at render-time.
-		// Alternatively, we can disallow changing "style" string at runtime and allow it to be set 
+		// Alternatively, we can disallow changing "style" string at runtime and allow it to be set
 		// at init-time only (as it was in pre-ares enyo).
 		//this.domStyles = {};
 		//this.addStyles(this.kindStyle);
@@ -419,7 +419,9 @@ enyo.kind({
 		}
 		if (this.hasNode()) {
 			this.renderDom();
-			this.rendered();
+			if (this.generated) {
+				this.rendered();
+			}
 		}
 		// return 'this' to support method chaining
 		return this;
@@ -446,7 +448,9 @@ enyo.kind({
 		// generate our HTML
 		pn.innerHTML = this.generateHtml();
 		// post-rendering tasks
-		this.rendered();
+		if (this.generated) {
+			this.rendered();
+		}
 		// support method chaining
 		return this;
 	},
@@ -470,7 +474,9 @@ enyo.kind({
 		this.setupOverflowScrolling();
 		document.write(this.generateHtml());
 		// post-rendering tasks
-		this.rendered();
+		if (this.generated) {
+			this.rendered();
+		}
 		// support method chaining
 		return this;
 	},
@@ -487,7 +493,9 @@ enyo.kind({
 		// post-render layout work *and* post-resize layout work.
 		this.reflow();
 		for (var i=0, c; (c=this.children[i]); i++) {
-			c.rendered(); 
+			if (c.generated) {
+				c.rendered();
+			}
 		}
 	},
 	/**
@@ -584,7 +592,7 @@ enyo.kind({
 		// but that has not actually happened at this point.
 		// We set 'generated = true' here anyway to avoid having to walk the
 		// control tree a second time (to set it later).
-		// The contract is that insertion in DOM will happen synchronously 
+		// The contract is that insertion in DOM will happen synchronously
 		// to generateHtml() and before anybody should be calling hasNode().
 		this.generated = true;
 		return h;
@@ -603,7 +611,7 @@ enyo.kind({
 		var results = '';
 		for (var i=0, c; (c=this.children[i]); i++) {
 			var h = c.generateHtml();
-			results += h; 
+			results += h;
 		}
 		return results;
 	},
@@ -757,7 +765,7 @@ enyo.kind({
 			Returns passed-in string with ampersand, less-than, and greater-than
 			characters replaced by HTML entities, e.g.,
 			'&lt;code&gt;"This &amp; That"&lt;/code&gt;' becomes
-			'&amp;lt;code&amp;gt;"This &amp;amp; That"&amp;lt;/code&amp;gt;' 
+			'&amp;lt;code&amp;gt;"This &amp;amp; That"&amp;lt;/code&amp;gt;'
 		*/
 		escapeHtml: function(inText) {
 			return inText != null ? String(inText).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') : '';
@@ -805,7 +813,7 @@ enyo.kind({
 		/**
 			Returns passed-in string with ampersand and double quote characters
 			replaced by HTML entities, e.g., 'hello from "Me & She"' becomes
-			'hello from &amp;quot;Me &amp;amp; She&amp;quot;' 
+			'hello from &amp;quot;Me &amp;amp; She&amp;quot;'
 		*/
 		escapeAttribute: function(inText) {
 			return !enyo.isString(inText) ? inText : String(inText).replace(/&/g,'&amp;').replace(/\"/g,'&quot;');
@@ -833,8 +841,8 @@ enyo.Control.subclass = function(ctor, props) {
 	// at kind declaration time, in the interest of efficiency
 	// and ease of use.
 	//
-	// However, the properties are no longer 'live' in prototypes 
-	// because of this magic--i.e., changes to the prototype of 
+	// However, the properties are no longer 'live' in prototypes
+	// because of this magic--i.e., changes to the prototype of
 	// a Control subclass will not necessarily be reflected in
 	// instances of that control (e.g., chained prototypes).
 	//
@@ -843,7 +851,7 @@ enyo.Control.subclass = function(ctor, props) {
 	//
 	var proto = ctor.prototype;
 	//
-	// 'kindClasses' comes either from our inheritance chain (e.g., proto's prototype chain) 
+	// 'kindClasses' comes either from our inheritance chain (e.g., proto's prototype chain)
 	// or has been forced by a kind declaration.
 	//
 	if (proto.classes) {
