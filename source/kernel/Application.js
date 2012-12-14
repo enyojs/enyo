d3a5d729f3fea7b64a8e4af47e85ada9bf78bf10
W. Cole Davis <wcole.davis@gmail.com>
2012-12-14 03:51:22 -0800

    attempting different approach to application object as controller

--- source/kernel/Application.js
+++ source/kernel/Application.js
@@ -1,141 +1,93 @@
-(function () {
-  
-  //*@public
-  /**
-    TODO: Docs
-  */
-  enyo.start = function () {
-    if (enyo.isRunning) return;
-    if (enyo.application) {
-      enyo.isRunning = true;
-      enyo.application.start();
-    } else {
-      enyo.warn("No application found, attempting to use default. " +
-        "This is a deprecated convention, please see enyo.App");
-      if (App) {
-        new App().renderInto(document.body);
-      } else throw "I can't find anything to run!";
-    }
-    enyo.flushStartupQueue();
-  };
-  
-  //*@protected
-  enyo.startupQueue = [];
-  
-  //*@public
-  /**
-    Execute the method (with the optional context) when the framework
-    has received the _enyo.start_ command. If the framework has already
-    started, the method will be exected immediately, otherwise it will
-    be added to a queue and flushed at the appropriate time.
-  */
-  enyo.run = function (fn, context) {
-    var q = enyo.startupQueue || [], args = enyo.toArray(arguments).slice(2);
-    if (!fn) return false;
-    if (enyo.isString(fn)) {
-      fn = enyo.getPath.call(context, fn);
-      if (!fn) return false;
-    }
-    if (!enyo.isFunction(fn)) return false;
-    if (enyo.isStarted) {
-      return fn.apply(context, args);
-    } else {
-      q.push(enyo.bind(context, function (fn, args) {
-        fn.apply(this, args);
-      }, fn, args));
-    }
-  };
-  
-  //*@protected
-  enyo.flushStartupQueue = function () {
-    var q = enyo.startupQueue, fn;
-    if (!q) return true;
-    while (q && q.length) {
-      fn = q.shift();
-      if (fn && enyo.isFunction(fn)) fn();
-    }
-  };
-  
-  //*@protected
-  /**
-    Central framework reference to the running application.
-  */
-  enyo.application = false;
-  
-  //*@public
-  /**
-    Creates and returns a reference to the _enyo.Application_ component.
-    This component is used internally by the framework. It is assumed that
-    there is only _one_ application instance. Accepts a hash of properties,
-    _inControl_, that define the root _enyo.Control_ for the application that
-    will be rendered when the framework is ready to start. If present, a
-    second hash (optional second parameter), _inProps_, may be provided
-    and overload the _enyo.Application_ class methods and properties.
-    
-    TODO: This needs to be extended to be able to show loaders...
-  */
-  enyo.App = function (inControl, inProps) {
-    var ns, n, r, c = inControl || {}, ap = inProps || {};
-    n = ap.name || c.name || "app";
-    
-    // if kinds were defined in the namespace of the application
-    // (which they should be, ideally) we preserve them
-    ns = enyo.global[n];
-    r = (enyo.global[n] = enyo.application = new enyo.Application(c, ap));
-    
-    // if there were kinds that pre existed our application
-    // move those back to the application namespace
-    if (ns) for (var k in ns) r[k] = ns[k];
-    return r;
-  };
-  
-  
-  //*@public
-  /**
-    _enyo.Application_ has built-in functionality that facilitates the
-    framework in properly organizing startup routines, proper application
-    namespace, and allows for any classes based off of _enyo.Object_ or
-    its subclasses to be loaded out of order.
-    
-    TODO: Document the other API/features of _enyo.Application_
-    TODO: Actually implement the features of _enyo.Application_
-  */
-  enyo.kind({
-    name: "enyo.Application",
-    kind: "enyo.UiComponent",
-    //*@protected
-    constructor: function (inControl) {
-      var c = inControl || enyo.defaultRootView;
-      if (enyo.isString(c)) c = enyo.getPath(c) || enyo.defaultRootView;
-      else if (!enyo.isFunction(c)) c = enyo.kind(enyo.mixin({
-        kind: "enyo.Control",
-        isRootView: true
-      }, c));
-      this.rootView = c;
-      this.inherited(arguments);
+
+enyo.kind({
+    name: "enyo.ViewController",
+    kind: "enyo.Controller",
+    view: null,
+    renderTarget: "document.body",
+    constructor: function () {
+        this.inherited(arguments);
     },
-    //*@protected
-    constructed: function (inControl, inProps) {
-      this.importProps(inProps);
-      
-      // NOTE: we are deliberately NOT calling create here
+    create: function () {
+        var view = this.view;
+    },
+    render: function () {
+        var target = this.get("target");
+        var ctor = this.get("viewKind");
+        var view = this.view = new ctor();
+        view.renderInto(target);
+    },
+    renderInto: function (target) {
+        this.set("renderTarget", target);
+        this.render();
+    },
+    target: enyo.Computed(function () {
+        var target = this.renderTarget;
+        if ("string" === typeof target) {
+            target = enyo.getPath(target);
+        }
+        if (!target) {
+            throw "Cannot find requested render target!";
+        }
+        return target;
+    }, "renderTarget"),
+    viewKind: enyo.Computed(function () {
+        var view = this.view;
+        if ("string" === typeof view) {
+            view = enyo.getPath(view);
+        }
+        if (!view) {
+            throw "Cannot find the requested view!";
+        }
+        return view;
+    }, "view")
+});
+
+enyo.kind({
+    name: "enyo.Application",
+    kind: "enyo.ViewController",
+    autoStart: true,
+    renderOnStart: false,
+    controllers: null,
+    concat: ["controllers"],
+    constructor: function () {
+        this.inherited(arguments);
+        if (true === this.autoStart) this.start();
+    },
+    initComponents: function () {
+        enyo.forEach(["controllers"], function (prop) {
+            var fn = prop + "Changed";
+            if ("function" === typeof this[fn]) this[fn]();
+        }, this);
     },
     start: function () {
-      var r = this.rootView;
-      
-      // we look for the global main function and if it exists
-      // go ahead and fire it
-      (function (m) {m()})(window.main || enyo.nop);
-      
-      // since we overloaded our constructed method, call create now
-      this.create();
-      
-      // setup the root view
-      r = this.rootView = new r();
-      r.set("owner", this);
-      this.addChild(r);
-      r.renderInto(document.body);
+        this.initComponents();
+        if (true === this.renderOnStart) {
+            this.render();
+        }
+    },
+    controllersChanged: function () {
+        var controllers = this.controllers;
+        enyo.forEach(controllers, function (kind) {
+            var ctor = enyo.constructorForKind(kind);
+            var instName = this.instanceNameFrom(kind, ctor);
+            var namespace = this.get("namespace");
+            if (!ctor) return enyo.warn("enyo.Application: " +
+                "could not find a constructor for the requested " +
+                "controller kind - " + kind);
+            namespace[instName] = new ctor();
+        }, this);
+    },
+    namespace: enyo.Computed(function () {
+        var kindName = this.kindName;
+        var parts = kindName.split(".");
+        var ns = parts[0];
+        return enyo.getPath(ns);
+    }),
+    instanceNameFrom: function (name, ctor) {
+        var instanceName = ctor.prototype.instanceName;
+        if ("string" === typeof instanceName) return instanceName;
+        name = name.indexOf(".") > -1? name.split(".")[1]: name;
+        name = enyo.uncap(name);
+        return (ctor.prototype.instanceName = name);
     }
-  });
-  
-}());
+});
