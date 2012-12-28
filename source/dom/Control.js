6b49977064a19c0839375891b45bdc218d5b0cf3
Ben Combee <combee@techwood.org>
2012-12-27 18:15:39 -0600

    Update source/dom/Control.js
    Restore old behavior of enyo.Control.getBounds() that hadn't been properly documented to fix layout code.
    
    Enyo-DCO-1.1-Signed-Off-By: Ben Combee (ben.combee@palm.com)

--- source/dom/Control.js
+++ source/dom/Control.js
@@ -520,14 +520,15 @@ enyo.kind({
 			{left: _offsetLeft_, top: _offsetTop_, width: _offsetWidth_, height: _offsetHeight_}
 
 		Values returned are only valid if _hasNode()_ is truthy.
-		If there's no DOM node for the object, this returns _null_.
+		If there's no DOM node for the object, this a structure with _undefined_ for all fields.
 
 			var bounds = this.getBounds();
 			enyo.log(bounds.width);
 	*/
 	getBounds: function() {
 		var n = this.node || this.hasNode();
-		return enyo.dom.getBounds(n);
+		var b = enyo.dom.getBounds(n);
+		return b || { return {left: undefined, top: undefined, width: undefined, height: undefined};
 	},
 	/**
 		Sets any or all of the geometry style properties _width_, _height_,
