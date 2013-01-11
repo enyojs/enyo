2adfafedfeb4bec13746860355e2eb573c0a8622
Cole Davis <cole.davis@palm.com>
2013-01-11 10:40:48 -0800

    update to function without enyo.call
    Enyo-DCO-1.1-Signed-off-by: Cole Davis <cole.davis@palm.com>

--- source/ajax/Async.js
+++ source/ajax/Async.js
@@ -68,12 +68,15 @@ enyo.kind({
 	},
 	handle: function(inValue, inHandlers) {
 		var r = inHandlers.shift();
+		var fn;
+		var v;
 		if (r) {
 			if (r instanceof enyo.Async) {
 				this.route(r, inValue);
 			} else {
 				// handler can return a new 'value'
-				var v = enyo.call(this.context || this, r, [this, inValue]);
+				fn = "function" === typeof r? r: enyo.nop;
+				v = fn.apply(this.context || this, [this, inValue]);
 				// ... but only if it returns something other than undefined
 				v = (v !== undefined) ? v : inValue;
 				// next handler
