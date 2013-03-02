8a6ebc2c53bc3a41ac44d1084f25df8fada8e6ff
David Greisen <dgreisen@gmail.com>
2013-03-02 18:08:43 -0500

    fixed rows/columns index mixup

--- source/ui/Table.js
+++ source/ui/Table.js
@@ -72,8 +72,9 @@ enyo.kind({
 			}
 			this.createComponent(tr);
 		}
-		
+
 		numCols = this.columns.length || this.rows[0].length || 0;
+		var numRows = this.rows.length;
 		for (i=0; i<numCols; i++) {
 			row = this.rows[i];
 			tr = { tag: "tr", components: [] };
