4311bd6a37146e796d77c272494f709f7a66e705
Amjad Mohamed <andhos@gmail.com>
2013-03-26 19:07:51 +0800

    Update Ajax.js
    Check if contentType **begins with** 'application/json' instead of checking if contentType **is** 'application/json'.
    
    Otherwise it sets the post body to '[Object object]' when contentType is set to 'application/json; charset=utf-8'.

--- source/ajax/Ajax.js
+++ source/ajax/Ajax.js
@@ -101,7 +101,7 @@ enyo.kind({
 			} else {
 				xhr_headers["Content-Type"] = this.contentType;
 				if (body instanceof Object) {
-					if (this.contentType === "application/json") {
+					if (this.contentType.indexOf("application/json") === 0) {
 						body = JSON.stringify(body);
 					} else if (this.contentType === "application/x-www-form-urlencoded") {
 						body = enyo.Ajax.objectToQuery(body);
