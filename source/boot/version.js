c7641672989eb8e56acf5e0e3e092730df2befba
Ben Combee <ben.combee@lge.com>
2013-07-09 15:06:16 -0500

    Update version number and scheme based on feedback from Gray
    Enyo-DCO-1.1-Signed-Off-By: Ben Combee (ben.combee@lge.com)

--- source/boot/version.js
+++ source/boot/version.js
@@ -1,6 +1,13 @@
-(function(enyo) {
+/*
+	Each Enyo JS-supplied library will annotate this structure with their
+	own versioning. In general, the core libraies of layout and onyx will
+	follow the core Enyo version number scheme, but secondary libraries
+	may change version at a different rate.
 
-	// next stable version is "2.3.0"
-	enyo.version = "2.3.0-alpha.pilot4";
+	Prereleases are indicated by "-pre.N" where N is incremented for each
+	prerelease. In-between versions have a "-dev" suffix.
+*/
 
-})(window.enyo);
+enyo.version = {
+	enyo: "2.3.0-pre.4-dev"
+};
