4134b4ee3aa558500e24b0e43dd9be7cc3064785
graynorton <gray@stanfordalumni.org>
2013-07-10 08:39:26 -0700

    Fleshing out explanatory text, bumping version number.
    Since we've already pushed out the fourth pre-release for 2.3, we're now working on 2.3.0-pre.5.

--- source/boot/version.js
+++ source/boot/version.js
@@ -1,13 +1,19 @@
 /*
-	Each Enyo JS-supplied library will annotate this structure with their
-	own versioning. In general, the core libraies of layout and onyx will
-	follow the core Enyo version number scheme, but secondary libraries
-	may change version at a different rate.
+	Each Enyo-provided library will annotate this structure with its own
+	version info. Why? In general, Enyo and its libraries are versioned
+	and released together, but libraries may be versioned separately in
+	the future. Also, since libraries are checked out and updated
+	individually, a project may be using "mismatched" libraries at any
+	given time (e.g., the last stable release of Enyo, but an
+	unreleased checkout from the master branch of layout).
 
-	Prereleases are indicated by "-pre.N" where N is incremented for each
-	prerelease. In-between versions have a "-dev" suffix.
+	Enyo uses semantic versioning. Official releases have version
+	numbers	of the form "X.y.z". Prereleases are indicated by a suffix
+	of the form "-pre.N" where N is incremented for each prerelease.
+	Between releases, version numbers include an additional "-dev"
+	suffix.
 */
 
 enyo.version = {
-	enyo: "2.3.0-pre.4-dev"
+	enyo: "2.3.0-pre.5-dev"
 };
