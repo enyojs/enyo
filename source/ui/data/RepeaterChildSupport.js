eefe4fe02a57e408f95bfd1c0b196db8e4c34f3e
Cole Davis <cole.davis@lge.com>
2013-07-31 19:26:49 -0700

    change selection  api
    Enyo-DCO-1.1-Signed-off-by: Cole Davis <cole.davis@lge.com>

--- source/ui/data/RepeaterChildSupport.js
+++ source/ui/data/RepeaterChildSupport.js
@@ -7,30 +7,39 @@ enyo.createMixin({
 	selectedChanged: function () {
 		if (this.repeater.selection) {
 			this.addRemoveClass(this.selectionClass || "selected", this.selected);
-			if (this.selected) {
-				this.bubble("onSelected");
-			} else {
-				this.bubble("onDeselected");
+			// for efficiency purposes we now directly call this method as opposed to
+			// forcing a synchronous event dispatch
+			if (this.selected && !this.repeater.isSelected(this.model)) {
+				this.repeater.select(this.index);
+			} else if (!this.selected && this.repeater.isSelected(this.model)) {
+				this.repeater.deselect(this.index);
 			}
 		}
 	},
 	decorateEvent: function (sender, event) {
 		event.model = this.model;
 		event.child = this;
+		event.index = this.index;
 		this.inherited(arguments);
-	},
-	create: function () {
-		this.__sbid = this.binding({from: ".model.selected", to: ".selected", twoWay: true, kind: "enyo.BooleanBinding"}).id;
-	},
-	destroy: function () {
-		var $b = enyo.Binding.find(this.__sbid);
-		if ($b) {
-			$b.destroy();
-		}
 	},
 	__selectionHandler: function (sender, event) {
 		if (this.repeater.selection) {
 			this.set("selected", !this.selected);
+		}
+	},
+	create: function () {
+		var r$ = this.repeater,
+			s$ = r$.selectionProperty;
+		if (s$) {
+			this.__sbid = this.binding({from: ".model." + s$, to: ".selected", kind: "enyo.BooleanBinding", twoWay: true}).id;
+		}
+	},
+	destroy: function () {
+		if (this.__sbid) {
+			var b$ = enyo.Binding.find(this.__sbid);
+			if (b$) {
+				b$.destroy();
+			}
 		}
 	}
 });
