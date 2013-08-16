a440529734a60d03498e019c4f81a3cfa0d4a2f0
Lex Podgorny <ap.public1@gmail.com>
2013-08-16 16:43:36 -0700

    Moving rtl to protected

--- source/dom/Control.js
+++ source/dom/Control.js
@@ -64,12 +64,14 @@ enyo.kind({
 		*/
 		fit: null,
 		//* Used by Ares design editor for design objects
-		isContainer: false,
-		//* Layout direction. Left-to-right (false) or right-to-left (true)
-		rtl: false
+		isContainer: false
 	},
 	//*@protected
 	concat: ["classes", "style", "attributes"],
+	//*@protected, 
+	//* Layout direction. Left-to-right (false) or right-to-left (true)
+	//* Should only be read by widget developers (sub-kinders), and normally never set by end developers
+	rtl: false,
 	//*@public
 	handlers: {
 		//* Controls will call a user-provided _tap_ method when tapped upon.
