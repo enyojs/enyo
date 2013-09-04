e9cee42f071a8329246ae08b1896454b4f25d509
anugrahsaxena <anugrah.saxena@lge.com>
2013-09-04 14:38:58 +0530

    GF-9695 AjaxSample Update
    Issue: Yahoo API upcoming.events has been removed which was giving the
    error message.
    Fixes:
    1) Used other working Yahoo APIs, e.g., weather.forecast and geo.places
    APIs.
    2) Used "onyx.Popup" to show failed scenarios. Provided proper error
    message.
    3) Refreshed textArea.

--- samples/AjaxSample.js
+++ samples/AjaxSample.js
@@ -5,14 +5,15 @@ enyo.kind({
 	components: [
 		{kind: "FittableColumns", classes:"onyx-toolbar-inline", components: [
 			{content: "YQL: "},
-			{kind: "onyx.Input", name:"query", fit:true, value:'select * from upcoming.events where woeid in (select woeid from geo.places where text="Sunnyvale, CA")'},
+			{kind: "onyx.Input", name:"query", fit:true, value:'select * from weather.forecast where woeid in (select woeid from geo.places where text="san francisco, ca")'},
 			{kind: "onyx.Button", content:"Fetch", ontap:"fetch"}
 		]},
 		{kind: "FittableColumns", classes:"onyx-toolbar-inline", components: [
 			{content: "URL: "},
 			{kind: "onyx.Input", name:"baseUrl", fit:true, value:'http://query.yahooapis.com/v1/public/yql?format=json'}
 		]},
-		{kind: "onyx.TextArea", fit:true, classes:"ajax-sample-source"}
+		{kind: "onyx.TextArea", fit:true, classes:"ajax-sample-source"},
+		{name: "basicPopup", kind: "onyx.Popup", centered: true, floating: true, classes:"onyx-sample-popup", style: "padding: 10px;", content: "Popup..."}
 	],
 	fetch: function() {
 		var ajax = new enyo.Ajax({
@@ -32,6 +33,12 @@ enyo.kind({
 		this.$.textArea.setValue(JSON.stringify(inResponse, null, 2));
 	},
 	processError: function(inSender, inResponse) {
-		window.alert("Error!");
+		var errorLog = "Error" + ": " + inResponse + "! " + (JSON.parse(inSender.xhrResponse.body)).error.description;
+		this.$.textArea.setValue(JSON.stringify(inSender.xhrResponse, null, 2));
+		this.showPopup(errorLog);
+	},
+	showPopup: function(errorLog) {
+		this.$.basicPopup.setContent(errorLog);
+		this.$.basicPopup.show();
 	}
 });
