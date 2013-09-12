227f45515c2a68513b9ed2db79a28c72389cdabd
Cole Davis <cole.davis@lge.com>
2013-09-11 22:44:24 -0700

    ensure the length event fires so bindings can be updated prior to the add event
    Enyo-DCO-1.1-Signed-off-by: Cole Davis <cole.davis@lge.com>

--- source/data/Collection.js
+++ source/data/Collection.js
@@ -236,10 +236,10 @@ enyo.kind({
 		rr.splice.apply(rr, rec);
 		// update the new length
 		this.length = rr.length;
-		// trigger the event with the indices
-		if (d.length) { this.triggerEvent("add", {records: d}); }
 		// now alert any observers of the length change
 		if (l != this.length) { this.notifyObservers("length", l, this.length); }
+		// trigger the event with the indices
+		if (d.length) { this.triggerEvent("add", {records: d}); }
 	},
 	/**
 		Accepts a record, or an array of records, to be removed from the collection.
