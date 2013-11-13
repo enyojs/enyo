7d8768984d2b24741d61d273fd5ee8f3b2472d1e
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2013-11-13 10:37:37 +0100

    rewrite touchGesture.connect to use addEventListener by default; fall back on document.onEvent on IE8

--- source/touch/touch.js
+++ source/touch/touch.js
@@ -117,11 +117,14 @@ enyo.requiresWindow(function() {
 			}
 		},
 		connect: function() {
-			enyo.forEach(['ontouchstart', 'ontouchmove', 'ontouchend', 'ongesturestart', 'ongesturechange', 'ongestureend'], function(e) {
-				document[e] = enyo.dispatch;
+			enyo.forEach(['touchstart', 'touchmove', 'touchend', 'gesturestart', 'gesturechange', 'gestureend'], function(e) {
+				if(enyo.platform.ie < 9){
+					document["on" + e] = enyo.dispatch;
+				} else {
+					// on iOS7 document.ongesturechange is never called
+					document.addEventListener(e, enyo.dispatch, false);
+				}
 			});
-			// on iOS7 document.ongesturechange is never called
-			document.addEventListener("gesturechange", enyo.dispatch, false);
 
 			if (enyo.platform.androidChrome <= 18 || enyo.platform.silk === 2) {
 				// HACK: on Chrome for Android v18 on devices with higher density displays,
