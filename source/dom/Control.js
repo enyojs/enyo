723e00b989f1eb026a4d598042765d87483ec92e
Gray Norton <gray.norton@lge.com>
2014-01-29 18:08:11 -0800

    Fixing typo.
    Enyo-DCO-1.1-Signed-Off-By: Gray Norton (gray.norton@lge.com)

--- source/dom/Control.js
+++ source/dom/Control.js
@@ -83,7 +83,7 @@ enyo.kind({
 	//*@protected
 	//* Layout direction. Left-to-right (false) or right-to-left (true)
 	//* Should only be read by widget developers (sub-kinders), and normally never set by end developers
-	rtl: false,s
+	rtl: false,
 	//*@public
 	handlers: {
 		//* Controls will call a user-provided _tap_ method when tapped upon.
