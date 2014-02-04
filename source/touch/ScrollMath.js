950b2d19b7b264ace0b859ab867ba8ba97b4e411
Kevin Schaaf <kevin.schaaf@lge.com>
2014-02-03 18:37:03 -0800

    Revert "GF-59604: Prevent firing onScrollStop from ScrollMath"
    This reverts commit 7f1a52b9b8c6102ee89d371613ab01d1efdfc688.

--- source/touch/ScrollMath.js
+++ source/touch/ScrollMath.js
@@ -212,7 +212,7 @@ enyo.kind({
 		this.y = this.y0 = y;
 		this.x = this.x0 = x;
 		this.scroll();
-		this.stop();
+		this.stop(true);
 	},
 	startDrag: function(e) {
 		this.dragging = true;
@@ -267,7 +267,7 @@ enyo.kind({
 	mousewheel: function(e) {
 		var dy = this.vertical ? e.wheelDeltaY || e.wheelDelta: 0;
 		if ((dy > 0 && this.y < this.topBoundary) || (dy < 0 && this.y > this.bottomBoundary)) {
-			this.stop();
+			this.stop(true);
 			this.y = this.y0 = this.y0 + dy;
 			this.start();
 			return true;
