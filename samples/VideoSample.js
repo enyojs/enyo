193a0e0c15cfe2d4ea594b9e26bd9757629da3a4
Aaron Tam <aaron.tam@lge.com>
2014-03-21 15:30:11 -0700

    ENYO-3914: Hide play image overlay when programmatically played, general cleanup.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- samples/VideoSample.js
+++ samples/VideoSample.js
@@ -11,81 +11,104 @@ enyo.kind({
 				src: "http://media.w3.org/2010/05/bunny/movie.mp4", 
 				onratechange: "rateChanged", 
 				ontimeupdate: "timeChanged",
+				ondurationchange: "durationChanged",
 				onFastforward: "playbackChanged",
 				onRewind: "playbackChanged",
 				onPlay: "playbackChanged",
 				ontap: "togglePlayback"
 			},
-			{kind: "enyo.Anchor", ontap: "anchorVideoTapped"}
+			{kind: "enyo.Anchor", name: "playOverlay", ontap: "playVideo"}
 		]},
-		{kind: "enyo.Button", content: "Play", ontap: "buttonPlayTapped"},
-		{kind: "enyo.Button", content: "Pause", ontap: "buttonPauseTapped"},
-		{kind: "enyo.Button", content: "<< Rewind", ontap: "buttonRewindTapped"},
-		{kind: "enyo.Button", content: "Fast Forward >>", ontap: "buttonFastForwardTapped"},
-		{kind: "enyo.Button", content: "< Jump Backward", ontap: "buttonJumpBackwardTapped"},
-		{kind: "enyo.Button", content: "Jump Forward >", ontap: "buttonJumpForwardTapped"},
-		{kind: "enyo.Button", content: "Toggle Loop", ontap: "buttonToggleLoopTapped"},
-		{name: "results", classes: "results"},
-		{kind: "enyo.Popup", name: "popupStatus", floating: true, centered: true, classes: "popup"}
+		{kind: "enyo.Button", content: "Play", ontap: "playVideo"},
+		{kind: "enyo.Button", content: "Pause", ontap: "pauseVideo"},
+		{kind: "enyo.Button", content: "<< RW", ontap: "buttonRewindTapped"},
+		{kind: "enyo.Button", content: "FF >>", ontap: "buttonFastForwardTapped"},
+		{kind: "enyo.Button", content: "< Jump", ontap: "buttonJumpBackwardTapped"},
+		{kind: "enyo.Button", content: "Jump >", ontap: "buttonJumpForwardTapped"},
+		{kind: "enyo.Button", content: "Loop", ontap: "buttonToggleLoopTapped"},
+		{name: "results", classes: "results", components: [
+			{classes: "result-section", components: [
+				{classes: "result-label", content: "Position:"},
+				{name: "videoPosition"}
+			]},
+			{classes: "result-section", components: [
+				{classes: "result-label", content: "Duration:"},
+				{name: "videoDuration"}
+			]},
+			{classes: "result-section", components: [
+				{classes: "result-label", content: "Action:"},
+				{name: "videoAction"}
+			]}
+		]}
+	],
+	bindings: [
+		{from: ".isPlaying", to: ".$.playOverlay.showing", transform: function(inValue) {
+			return !inValue;
+		}}
 	],
 	playbackChanged: function(inSender, inEvent) {
 		if (inEvent.playbackRate > 1) {
-			this.displayPopup("Fast-Forward");			
+			this.$.videoAction.setContent("Fast-Forward");			
 		} else if (inEvent.playbackRate < -1) {
-			this.displayPopup("Rewind");
+			this.$.videoAction.setContent("Rewind");
 		} else if (inEvent.playbackRate == 1) {
-			this.displayPopup("Play");
+			this.$.videoAction.setContent("Play");
 		}
+		return true;
 	},
 	togglePlayback: function(inSender, inEvent) {
-		if (inSender.isPaused()) {
-			inSender.play();
+		if (this.get("isPlaying")) {
+			this.pauseVideo(arguments);
 		} else {
-			inSender.pause();
-			this.displayPopup("Pause");
+			this.playVideo(arguments);
 		}
+		return true;
 	},
-	anchorVideoTapped: function(inSender, inEvent) {
-		inSender.hide();
+	playVideo: function(inSender, inEvent) {
+		this.set("isPlaying", true);
 		this.$.video.play();
+		this.$.videoAction.setContent("Play");
+		return true;
 	},
-	buttonPlayTapped: function(inSender, inEvent) {
-		this.$.video.play();
-	},
-	buttonPauseTapped: function(inSender, inEvent) {
+	pauseVideo: function(inSender, inEvent) {
+		this.set("isPlaying", false);
 		this.$.video.pause();
-		this.displayPopup("Pause");
+		this.$.videoAction.setContent("Pause");
+		return true;
 	},
 	buttonRewindTapped: function(inSender, inEvent) {
 		this.$.video.rewind();
+		return true;
 	},
 	buttonFastForwardTapped: function(inSender, inEvent) {
 		this.$.video.fastForward();
+		return true;
 	},
 	buttonJumpBackwardTapped: function(inSender, inEvent) {
 		this.$.video.jumpBackward();
-		this.displayPopup("Jump Backward " + this.$.video.getJumpSec() + "s");
+		this.$.videoAction.setContent("Jump Backward " + this.$.video.getJumpSec() + "s");
+		return true;
 	},
 	buttonJumpForwardTapped: function(inSender, inEvent) {
 		this.$.video.jumpForward();
-		this.displayPopup("Jump Forward " + this.$.video.getJumpSec() + "s");
+		this.$.videoAction.setContent("Jump Forward " + this.$.video.getJumpSec() + "s");
+		return true;
 	},
 	buttonToggleLoopTapped: function(inSender, inEvent) {
 		this.$.video.setLoop(!this.$.video.getLoop());
-		this.displayPopup("Looping " + (this.$.video.getLoop() ? "Enabled" : "Disabled"));
+		this.$.videoAction.setContent("Looping " + (this.$.video.getLoop() ? "Enabled" : "Disabled"));
+		return true;
 	},
 	rateChanged: function(inSender, inEvent) {
-		this.displayPopup("Playback " + inEvent.playbackRate + "x");
+		this.$.videoAction.setContent("Playback " + inEvent.playbackRate + "x");
+		return true;
 	},
 	timeChanged: function(inSender, inEvent) {
-		this.$.results.setContent("Duration: " + Math.floor(inEvent.duration) + "s, Current Position: " + Math.floor(inEvent.currentTime) + "s");
+		this.$.videoPosition.setContent(Math.floor(inEvent.currentTime) + "s");
+		return true;
 	},
-	displayPopup: function(inContent) {
-		var popup = this.$.popupStatus;
-		popup.setContent(inContent);
-		popup.setShowing(true);
-		enyo.job("autoHidePopup", function() { 
-			popup.hide(); 
-		}, 1000);
+	durationChanged: function(inSender, inEvent) {
+		this.$.videoDuration.setContent((inEvent.target && inEvent.target.duration ? Math.floor(inEvent.target.duration) : 0) + "s");
+		return true;
 	}
 });
