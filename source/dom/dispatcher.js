3534b79aa0ee9e284394ca69b7548c1bf691e247
Aaron Tam <aaron.tam@lge.com>
2014-03-24 14:58:43 -0700

    ENYO-3928: Expose API for triggering event handler on a control.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- source/dom/dispatcher.js
+++ source/dom/dispatcher.js
@@ -108,6 +108,11 @@
 			var type = e.type;
 			type = e.customEvent ? type : "on" + type;
 			return c.bubble(type, e, c);
+		},
+		triggerHandler: function() {
+			var args = Array.prototype.slice.call(arguments, 0),
+				control = args.shift();
+			return control.dispatchEvent(args);
 		}
 	};
 
@@ -123,6 +128,18 @@
 
 	enyo.dispatch = function(inEvent) {
 		return enyo.dispatcher.dispatch(inEvent);
+	};
+
+	/**
+		Triggers the handler for a given event type on the specified control.
+		The _inControl_ parameter is the control object, and the _inEventType_ parameter is the event type string.
+		The _inEvent_ and _inSender_ parameters are optional and represent event and sender objects, respectively,
+		to pass to the handler. For example:
+
+		enyo.triggerHandler(myControl, "ontap");
+	*/
+	enyo.triggerHandler = function(/* inControl, inEventType, [inEvent], [inSender] */) {
+		return enyo.dispatcher.triggerHandler.apply(this, arguments);
 	};
 
 	enyo.bubble = function(inEvent) {
