faff9c50fd64aa8695a7ff8059ccf1fb153f786d
Aaron Tam <aaron.tam@lge.com>
2014-03-27 17:35:10 -0700

    ENYO-3938: Gracefully handle/prevent circular reference errors when stringifying objects.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- source/kernel/log.js
+++ source/kernel/log.js
@@ -23,6 +23,18 @@ enyo.logging = {
 		return a$;
 	},
 	*/
+	validateArgs: function(inArr) {
+		// gracefully handle and prevent circular reference errors in objects
+		for (var i=0, l=inArr.length, item; (item=inArr[i]) || i<l; i++) {
+			try {
+				if (typeof item === "object") {
+					inArr[i] = enyo.json.stringify(item);
+				}
+			} catch (e) {
+				inArr[i] = "Error: " + e.message;
+			}
+		}
+	},
 	_log: function(inMethod, inArgs) {
 		// avoid trying to use console on IE instances where the object hasn't been
 		// created due to the developer tools being unopened
@@ -32,17 +44,16 @@ enyo.logging = {
         }
 		//var a$ = enyo.logging.formatArgs(inMethod, inArgs);
 		var a$ = enyo.isArray(inArgs) ? inArgs : enyo.cloneArray(inArgs);
+		enyo.logging.validateArgs(a$);
 		if (enyo.dumbConsole) {
 			// at least in early versions of webos, console.* only accept a single argument
 			a$ = [a$.join(" ")];
 		}
 		var fn = console[inMethod];
-		// We prevent calling fn.apply or console.log.apply in Firefox for Android due to a browser bug.
-		// FIXME: Remove Firefox for Android exclusion when issue is fixed in browser.
-		if (fn && fn.apply && !enyo.platform.androidFirefox) {
+		if (fn && fn.apply) {
 			// some consoles support 'warn', 'info', and so on
 			fn.apply(console, a$);
-		} else if (console.log.apply && !enyo.platform.androidFirefox) {
+		} else if (console.log.apply) {
 			// some consoles support console.log.apply
 			console.log.apply(console, a$);
 		} else {
