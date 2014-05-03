b48912968ee3ce204cd0cd1456ed4c647f99610b
Jason Robitaille <robitaille88@gmail.com>
2014-05-02 22:20:51 -0500

    Syntax fix

--- tools/minifier/minify.js
+++ tools/minifier/minify.js
@@ -278,7 +278,7 @@
 
 	w(opt);
 	var libPath = (opt.enyo.indexOf("lib"+path.sep+"enyo")>=0 ? opt.enyo+"/.." : opt.enyo+"/../lib");
-	walker.init(opt.enyo, opt.lib || "lib", opt.mapfrom, opt.mapto);
+	walker.init(opt.enyo, opt.lib || libPath, opt.mapfrom, opt.mapto);
 	walker.walk(path.basename(opt.packagejs), walkerFinished);
 
 })();
