582741c0caa304b2b08927d80ca1064508ed4c0b
Cole Davis <cole.davis@lge.com>
2014-06-02 15:21:16 -0700

    add support for grunt to execute jshint, mocha-phantomjs and phantomjs against our older unit-tests
    Enyo-DCO-1.1-Signed-Off-By: Cole Davis (cole.davis@lge.com)

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,26 @@ module.exports = function (grunt) {
 			// initially we're only looking at the source directory but will need to add all of
 			// the unit tests as well
 			all: ['source/**/*.js']
+		},
+		
+		mocha_phantomjs: {
+			all: ['tools/mocha-tests/index.html']
+		},
+		
+		exec: {
+			
+			// need to still execute the older tests
+			core: './node_modules/.bin/phantomjs tools/test/core/phantomjs-test.js'
 		}
 		
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-mocha-phantomjs');
 	
-	grunt.registerTask('test', ['jshint']);
+	// currently this is only being used because we are still running the older unit-tests
+	grunt.loadNpmTasks('grunt-exec');
+	
+	grunt.registerTask('test', ['jshint', 'mocha_phantomjs', 'exec:core']);
 	
 };
