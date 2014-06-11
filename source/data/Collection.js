d7ad7f543dca4bbb35ecdb9c774d2f73fadb4c25
Cole Davis <cole.davis@lge.com>
2014-06-11 14:25:37 -0700

    interim commit normalizing functionality and adding documentation
    Enyo-DCO-1.1-Signed-Off-By: Cole Davis (cole.davis@lge.com)

--- source/data/Collection.js
+++ source/data/Collection.js
@@ -9,6 +9,8 @@
 		Source = enyo.Source;
 	
 	/**
+		An array-like structure designed to house a _collection_ of {@link enyo.Model} instances.
+	
 		@public
 		@class enyo.Collection
 		@extends enyo.Component
@@ -32,52 +34,168 @@
 		noDefer: true,
 		
 		/**
+			The {@link enyo~kind) of {@link enyo.Model} that this {@link enyo.Collection} will be
+			implementing or housing. This is important to set properly so that when
+			[fetching]{@link enyo.Collection#fetch} the returned data will correctly be instanced
+			as the correct {@link enyo.Model} subclass.
+			
+			@type {(enyo.Model|string)}
+			@default enyo.Model
 			@public
 		*/
 		model: Model,
 		
 		/**
-			These are the default options used by the {@link Collection} in various routines. See their individual
-			definitions. There are multiple ways to use these or overload them...
-		
+			The configurable default {@link enyo.Collection#options}. These values will be used to
+			modify the behavior of the {@link enyo.Collection} unless additional _options_ are
+			passed into the methods that use them. When modifying these values in a
+			[subclass]{@link enyo~kind} of {@link enyo.Collection} they will be merged with existing
+			values.
+			
+			@type {object}
 			@public
 		*/
-		options: {
+		options: /** @lends enyo.Collection#options */ {
 			
 			/**
-				Merge datasets that are added when an instance of the record is already found in this
-				collection or in the store.
+				When data is being added to the {@link enyo.Collection} that already exist
+				(matched by {@link enyo.Model#primaryKey}) set the new data values with the current
+				{@link enyo.Model} instance. This indicates that it will update the existing values
+				with the new ones by calling {@link enyo.Model#set} on the
+				[model instance]{@link enyo.Model}.
 				
+				@type {boolean}
+				@default true
 				@public
 			*/
 			merge: true,
+			
+			/**
+				Many accessor methods of the {@link enyo.Collection} will emit events and/or
+				notifications. This indicates whether or not to supress those events or
+				notifications in times when that behavior is necessary. Most often you will not
+				want to modify this value.
+			
+				@type {boolean}
+				@default false
+				@public
+			*/
 			silent: false,
+			
+			/**
+				When [adding]{@link enyo.Collection#add} [models]{@link enyo.Model} this flag
+				indicates whether or not to remove (_purge_) the existing [models]{@link enyo.Model}
+				that are not included in the new dataset.
+			
+				@type {boolean}
+				@default false
+				@public
+			*/
 			purge: false,
+			
+			/**
+				The collection's [parse]{@link enyo.Collection#parse} method can automatically be
+				executed for incoming data added via the {@link enyo.Collection#constructor} method
+				or later after having [fetched]{@link enyo.Collection#fetch} data. It may be
+				necessary to distinguish these two occassions (one needing to parse and one not) by
+				using the runtime configuration options of the methods. In cases where it will
+				always be necessary this can be set to `true`.
+			
+				@type {boolean}
+				@default false
+				@public
+			*/
 			parse: false,
+			
+			/**
+				When data being added to the {@link enyo.Collection} cannot be found (or 
+				{@link enyo.Collection#options#find} is `false`) this descides if a new
+				{@link enyo.Model} should be created. [Models]{@link enyo.Model} that are created
+				by an {@link enyo.Collection} have their {@link enyo.Model#owner} property set to
+				the {@link enyo.Collection} that instanced them. This is important because if the
+				{@link enyo.Collection} is [destroyed]{@link enyo.Collection#destroy} it will also
+				[destroy]{@link enyo.Model#destroy} [models]{@link enyo.Model} that it owns.
+			
+				@type {boolean}
+				@default true
+				@public
+			*/
 			create: true,
+			
+			/**
+				When data being added to the {@link enyo.Collection} is not already an
+				{@link enyo.Model} instance it will attempt to find an existing model by its
+				{@link enyo.Model#primaryKey} if it exists. In most cases this is the prefered
+				behavior but if the {@link enyo.Model} class being instanced does not have a
+				{@link enyo.Model#primaryKey} it is unnecessary and this value can be set to
+				`false`.
+				
+				@type {boolean}
+				@default true
+				@public
+			*/
 			find: true,
+			
+			/**
+				When [adding]{@link enyo.Collection#add} [models]{@link enyo.Model} to the
+				{@link enyo.Collection} it can also be sorted. If the
+				{@link enyo.Collection#comparator} is a _function_ and this value is `true` it will
+				use the _comparator_ to sort the entire {@link enyo.Collection}. It can also be
+				a _function_ that will be used to sort the {@link enyo.Collection} instead of or in-
+				place of a {@link enyo.Collection#comparator}.
+			
+				@type {(boolean|function)}
+				@default false
+				@public
+			*/
 			sort: false,
+			
+			/**
+				When modifications are made to the {@link enyo.Collection} this flag will ensure
+				that those changes are [committed]{@link enyo.Collection#commit} according to the
+				configuration and {@link enyo.Collection#source}. This can also be configured per-
+				call to methods that use it.
+			
+				@type {boolean}
+				@default false
+				@public
+			*/
 			commit: false,
 			
 			/**
-				Used in {@link #remove} to indicate whether or not to destroy models that are removed from
-				the {@link Collection}.
+				When [models]{@link enyo.Model} are [removed]{@link enyo.Collection#remove} from the
+				dataset this flag indicates whether or not to {@link enyo.Model#destroy} them as
+				well. Note that this could have a significant impact if the same
+				[models]{@link enyo.Model} are used in other [collections]{@link enyo.Collection}.
 			
+				@type {boolean}
+				@default false
 				@public
 			*/
 			destroy: false,
 			
 			/**
-				Used in {@link #remove} to indicate whether or not to remove models from the {@link #store}
-				as well as the {@link Collection}. This flag is ignored is {@link #options.destroy} is `true`.
-				
+				When [models]{@link enyo.Model} are [removed]{@link enyo.Collection#remove} from the
+				dataset this flag indicates whether or not to also _remove_ them from the
+				{@link enyo.Collection#store}. This is rarely necessary and can cause problems if
+				the [models]{@link enyo.Model} are used in other
+				[collections]{@link enyo.Collection}. It is also ignored if the
+				{@link enyo.Collection#options#destroy} flag is `true`.
+			
+				@type {boolean}
+				@default false
 				@public
 			*/
 			complete: false,
 			
 			/**
-				Automatically fetch after initialization.
+				When the {@link enyo.Collection} is initialized it can automatically attempt to
+				{@link enyo.Collection#fetch} data when the {@link enyo.Collection#source} and
+				{@link enyo.Collection#url} or {@link enyo.Collection#buildUrl} properties are
+				configured properly.
 				
+				@type {boolean}
+				@default false
 				@public
 			*/
 			fetch: false
@@ -96,16 +214,68 @@
 		],
 		
 		/**
-			@public
+			Modify the structure of data such that it can be used by the {@link enyo.Collection#add}
+			method. This method will only be used during initialization or after a successful
+			{@link enyo.Collection#fetch} if the {@link enyo.Collection#options#parse} flag is set
+			to `true`. This can be used for simple remapping, renaming or complex restructuring of
+			data coming from a {@link enyo.Collection#source} that needs to be modified prior to
+			being [added]{@link enyo.Collection#add} to the dataset.
+		
+			@param {*} data The incoming data passed to the {@link enyo.Collection#constructor} or
+				returned by a successful {@link enyo.Collection#fetch}.
+			@returns {array} The properly formatted data to be accepted by
+				{@link enyo.Collection#add} method.
 			@method
+			@public
 		*/
 		parse: function (data) {
 			return data;
 		},
 		
 		/**
-			@public
+			The configuration options for {@link enyo.Collection#add}. For complete descriptions of
+			the options and their defaults {@see enyo.Collection#options}. Some properties have a
+			different meaning in a specific context. Please review their descriptions below to see
+			how they are used in this context.
+		
+			@typedef {object} enyo.Collection#add~options
+			@property {boolean} merge - Update existing models when found.
+			@property {boolean} purge - Remove existing models not in new dataset.
+			@property {boolean} silent - Emit events and notifications.
+			@property {boolean} parse - Parse the incoming dataset before evaluating.
+			@property {boolean} find - Look for an existing model.
+			@property {(boolean|function)} sort - Sort the finalized dataset.
+			@property {boolean} commit - {@link enyo.Collection#commit} changes to the
+				{@link enyo.Collection} after completing the {@link enyo.Collection#add}.
+			@property {boolean} create - When an existing {@link enyo.Model} instance cannot be
+				resolved it should _create_ a new instance.
+			@property {number} index - The index at which to add the new dataset. Defaults to the
+				end of the current dataset if not explicitly set or valid.
+			@property {object} modelOptions - When instancing a model this object will be passed
+				to the constructor as its {@link enyo.Model#constructor} options parameter.
+		*/
+		
+		/**
+			Add data to the dataset. This method can add an individual [model]{@link enyo.Model} or
+			an array of [models]{@link enyo.Model}. It can splice them into the dataset at a
+			designated index or remove models from the existing dataset that are not included in the
+			new one. {@see enyo.Collection#add~options} for detailed information on the
+			configuration options available for this method. This method is heavily optimized for
+			batch operations on _arrays_ of data. For better performance ensure that loops do not
+			consecutively call this method but instead build an array to pass to it as its first
+			parameter.
+			
+			@fires enyo.Collection#add
+			@param {(object|object[]|enyo.Model|enyo.Model[])} models The data to add to the
+			 	{@link enyo.Collection} that can be an object-literal, an array of object-literals,
+				an {@link enyo.Model} instance or array of {@link enyo.Model} instances. Note if the
+				{@link enyo.Collection#options#parse} configuration option is `true` it will use the
+				returned value as this parameter.
+			@param {enyo.Collection#add~options} [opts] The configuration options that modify the
+				behavior of this method. The [defaults]{@link enyo.Collection#options} will be
+				merged with these options before evaluating.
 			@method
+			@public
 		*/
 		add: function (models, opts) {
 			var loc = this.models
@@ -133,7 +303,11 @@
 				, find = opts.find
 				, sort = opts.sort
 				, commit = opts.commit
-				, create = opts.create !== false;
+				, create = opts.create !== false
+				, modelOpts = opts.modelOptions
+				, index = opts.index;
+				
+			idx = !isNaN(index) ? Math.max(0, Math.min(len, index)) : idx;
 
 			/*jshint -W018 */
 			sort && !(typeof sort == 'function') && (sort = this.comparator);
@@ -141,12 +315,15 @@
 			
 			// for a special case purge to remove records that aren't in the current
 			// set being added
+			
+			if (parse) models = this.parse(models);
 				
 			// we treat all additions as an array of additions
 			!(models instanceof Array) && (models = [models]);
 			
 			for (var i=0, end=models.length; i<end; ++i) {
 				model = models[i];
+				attrs = null;
 				
 				if (!model && isNaN(model)) continue;
 				
@@ -165,7 +342,7 @@
 					
 				
 				// see if we have an existing entry for this model/hash
-				found = loc.has(id);
+				if (find) found = loc.has(id);
 				
 				// if it already existed...
 				if (found) {
@@ -175,7 +352,6 @@
 					
 					if (merge) {
 						attrs || (attrs = model.attributes);
-						parse && (attrs = found.parse(attrs));
 						found.set(attrs, opts);
 					}
 					// with the purge flag we endeavor on the expensive track of removing
@@ -202,21 +378,30 @@
 					added.push(model);
 					this.prepareModel(model);
 				} else if (create) {
-					model = this.prepareModel(attrs || model, opts);
+					model = this.prepareModel(attrs || model, modelOpts);
 					added || (added = []);
 					added.push(model);
+					
+					// with the purge flag we endeavor on the expensive track of removing
+					// those models currently in the collection that aren't in the incoming
+					// dataset and aren't being created
+					if (purge) {
+						keep || (keep = {length: 0});
+						keep[model.euid] = model;
+						keep.length++;
+					}
 				}
 			}
 			
 			// here we process those models to be removed if purge was true
 			// the other guard is just in case we actually get to keep everything
 			// so we don't do this unnecessary pass
-			if (purge && ((keep && keep.length < len) || models.length === 0)) {
+			if (purge && (keep && keep.length)) {
 				removed || (removed = []);
 				keep || (keep = {});
 				for (i=0; i<len; ++i) !keep[(model = loc[i]).euid] && removed.push(model);
 				// if we removed any we process that now
-				removed.length && this.remove(removed, options);
+				removed.length && this.remove(removed, opts);
 			}
 			
 			// added && loc.stopNotifications().add(added, idx).startNotifications();
@@ -245,8 +430,15 @@
 		},
 		
 		/**
-			@public
+			Remove data from the dataset. It can take a [model]{@link enyo.Model} or an array of
+			[models]{@link enyo.Model}. If any of the instances are present in the
+			{@link enyo.Collection} they will be removed, in the order in which they are
+			encountered. Emits the {@link enyo.Collection#remove} event if any models were found and
+			removed from the dataset (and the `silent` option is not `true`).
+		
+			@fires enyo.Collection#remove
 			@method
+			@public
 		*/
 		remove: function (models, opts) {
 			var loc = this.models
--- tools/mocha-tests/tests/Collection.js
+++ tools/mocha-tests/tests/Collection.js
@@ -1,15 +1,500 @@
 describe('enyo.Collection', function () {
 	
 	var Collection = enyo.Collection,
-		proto = Collection.prototype;
+		Model = enyo.Model;
+	
+	var proto = Collection.prototype;
 	
 	describe('methods', function () {
 		
 		describe('#add', function () {
 			
+			it ('should respond to the method add', function () {
+				expect(proto).to.itself.respondTo('add');
+			});
+			
+			describe('params', function () {
+				
+				var collection;
+				
+				before(function () {
+					collection = new Collection();
+				});
+				
+				after(function () {
+					collection.destroy({destroy: true});
+				});
+				
+				describe('@models', function () {
+					
+					beforeEach(function () {
+						collection.empty({destroy: true});
+					});
+					
+					it ('should accept a model instance', function () {
+						collection.add(new enyo.Model());
+						
+						expect(collection.length).to.equal(1);
+					});
+					
+					it ('should accept an array of model instances', function () {
+						var ary = [
+							new Model(),
+							new Model(),
+							new Model()
+						];
+						
+						collection.add(ary);
+						
+						expect(collection.length).to.equal(3);
+					});
+					
+					it ('should accept an object literal if create is true (default)', function () {
+						collection.add({});
+						
+						expect(collection.length).to.equal(1);
+					});
+					
+					it ('should accept an array of object literals', function () {
+						var ary = [
+							{},
+							{},
+							{}
+						];
+						
+						collection.add(ary);
+						
+						expect(collection.length).to.equal(3);
+					});
+					
+				});
+				
+				describe('@opts', function () {
+					
+					var collection;
+					
+					before(function () {
+						collection = new Collection();
+					});
+					
+					after(function () {
+						collection.destroy({destroy: true});
+					});
+					
+					describe('~merge', function () {
+						
+						afterEach(function () {
+							collection.empty({destroy: true});
+						});
+						
+						it ('should merge existing (found) models with new data when true',
+							function () {
+							
+							// add a default model with a default id and some param to be updated
+							// if found and possible to merge
+							collection.add({id: 0, param: 0});
+							
+							// to test the merge interaction with some merged some not merged
+							collection.add([{id: 0, param: 1}, {id: 1}]);
+							
+							expect(collection.length).to.equal(2);
+							expect(collection.at(0).get('param')).to.equal(1);
+						});
+						
+						it ('should ignore existing models when false', function () {
+							
+							// add a default model with a default id and some param to be updated
+							// if found and possible to merge
+							collection.add({id: 0, param: 0});
+							
+							// to test the merge interaction with some merged some not merged
+							collection.add([{id: 0, param: 1}, {id: 1}], {merge: false});
+							
+							expect(collection.length).to.equal(2);
+							expect(collection.at(0).get('param')).to.equal(0);
+						});
+						
+					});
+					
+					describe('~purge', function () {
+						
+						afterEach(function () {
+							collection.empty({destroy: true});
+						});
+						
+						it ('should remove models from the existing dataset that are not ' +
+							'included in the new dataset', function () {
+							
+							// add a default model
+							collection.add({id: 0});
+							
+							// to test the merge interaction with purge
+							collection.add([{id: 1}, {id: 2}], {purge: true});
+							
+							expect(collection.length).to.equal(2);
+							expect(collection.at(0).get('id')).to.equal(1);
+							expect(collection.at(1).get('id')).to.equal(2);
+						});
+						
+					});
+					
+					describe('~silent', function () {
+						
+						afterEach(function () {
+							collection.empty({destroy: true});
+						});
+						
+						it ('should not emit events or notifications if the silent flag is true',
+							function () {
+							
+							var spy = sinon.spy();
+							
+							// before we test we actually want to add some items to check the more
+							// complex scenario where it would emit several
+							collection.add([{id: 0, param: 0}, {id: 1}]);
+							
+							// register the spy for any possible event to be emitted from the
+							// addition
+							collection.on('*', spy);
+							
+							// register an observer for any notifications emitted from the addition
+							collection.observe('*', spy);
+							
+							// this should ultimately trigger an add and remove and a change event
+							collection.add([{id: 0, param: 1}, {id: 2}], {
+								purge: true,
+								destroy: true,
+								silent: true
+							});
+							
+							expect(collection.length).to.equal(2);
+							expect(collection.at(0).get('param')).to.equal(1);
+							expect(collection.at(0).get('id')).to.equal(0);
+							expect(collection.at(1).get('id')).to.equal(2);
+							expect(spy.callCount).to.equal(0);
+							
+							collection.unobserve('*', spy);
+							collection.off('*', spy);
+						});
+						
+					});
+					
+					describe('~parse', function () {
+						
+						afterEach(function () {
+							collection.empty({destroy: true});
+						});
+						
+						it ('should call the collection\'s parse method if set to true before ' +
+							'evaluating the @models parameter', function () {
+							
+							var stub = sinon.stub(collection, 'parse');
+							
+							collection.add({}, {parse: true});
+							
+							expect(stub.callCount).to.equal(1);
+							
+							stub.restore();
+						});
+						
+					});
+					
+					describe('~find', function () {
+						
+						afterEach(function () {
+							collection.empty({destroy: true});
+						});
+						
+						it ('should look for an existing instance of the model being added in ' +
+							'the collection and store before instancing it', function () {
+							
+							var spy = sinon.spy(),
+								model = new Model({id: 1}, null, {noAdd: true});
+							
+							// we pre-add a model to the collection
+							collection.add({id: 0});
+							
+							// we pre-add a model to the store
+							enyo.store.add(model);
+							
+							// this spy should not be called but once
+							collection.on('add', spy);
+							
+							// simple test, re-add the same record and it should do nothing
+							collection.add({id: 0});
+							
+							// should add to the collection but not create a new one
+							collection.add({id: 1});
+							
+							expect(spy.callCount).to.equal(1);
+							expect(collection.length).to.equal(2);
+							expect(collection.at(1)).to.eql(model);
+							collection.off('add', spy);
+							model.destroy();
+						});
+						
+						it ('should ignore existing models and create new instances when false ' +
+							'and merge is false', function () {
+							
+								var spy = sinon.spy(),
+									model = new Model({id: 1}, null, {noAdd: true});
+							
+								// we pre-add a model to the collection
+								collection.add({id: 0});
+							
+								// we pre-add a model to the store
+								enyo.store.add(model);
+							
+								// this spy should not be called but once
+								collection.on('add', spy);
+								
+								// simple test, re-add the same record and it should do nothing
+								collection.add({id: 0}, {find: false, merge: false});
+							
+								// should add to the collection but not create a new one
+								collection.add({id: 1}, {find: false});
+							
+								expect(spy.callCount).to.equal(2);
+								expect(collection.length).to.equal(3);
+								expect(collection.at(1)).to.not.eql(model);
+								collection.off('add', spy);
+								model.destroy();
+						});
+						
+					});
+					
+					describe('~sort', function () {
+						
+						before(function () {
+							collection.comparator = function (a, b) {
+								if (a.get('id') > b.get('id')) return 1;
+								else return -1;
+							};
+						});
+						
+						after(function () {
+							collection.comparator = null;
+						});
+						
+						afterEach(function () {
+							collection.empty({destroy: true});
+						});
+						
+						it ('should use the comparator method of the collection if true',
+							function () {
+							
+							collection.add([
+								{id: 200},
+								{id: 201},
+								{id: 3},
+								{id: 3000},
+								{id: 456}
+							], {sort: true});
+							
+							// using the comparator we added this should have reversed their
+							// order so we should see smallest -> largest id ascending order
+							expect(collection.map(function (ln) {
+								return ln.get('id');
+							}).join(',')).to.equal('3,200,201,456,3000');
+						});
+						
+						it ('should use the provided method if a function', function () {
+							
+							var fn = function (a, b) {
+								if (a.get('id') > b.get('id')) return -1;
+								else return 1;
+							};
+							
+							collection.add([
+								{id: 200},
+								{id: 201},
+								{id: 3},
+								{id: 3000},
+								{id: 456}
+							], {sort: fn});
+							
+							// using the comparator we added this should have reversed their
+							// order so we should see smallest -> largest id ascending order
+							expect(collection.map(function (ln) {
+								return ln.get('id');
+							}).join(',')).to.equal('3000,456,201,200,3');
+						});
+						
+					});
+					
+					describe('~commit', function () {
+						
+						afterEach(function () {
+							collection.empty({destroy: true});
+						});
+						
+						it ('should call commit when commit is true and changes are made',
+							function () {
+							
+							var stub = sinon.stub(collection, 'commit');
+							
+							collection.add({}, {commit: true});
+							
+							expect(stub.callCount).to.equal(1);
+							stub.restore();
+						});
+						
+						it ('should not call commit when commit is true but changes are not made',
+							function () {
+							
+							var stub = sinon.stub(collection, 'commit');
+							
+							collection.add({id: 0});
+							
+							collection.add({id: 0}, {merge: false, commit: true});
+							
+							expect(stub.callCount).to.equal(0);
+							stub.restore();
+						});
+						
+					});
+					
+					describe('~create', function () {
+						
+						afterEach(function () {
+							collection.empty({destroy: true});
+						});
+						
+						it ('should create an instance if it cannot resolve a model', function () {
+							
+							collection.add({});
+							expect(collection.length).to.equal(1);
+						});
+						
+						it ('should not create an instance if it cannot resolve a model and the ' +
+							'create flag is false', function () {
+							
+							collection.add({}, {create: false});
+							expect(collection.length).to.equal(0);
+						});
+						
+					});
+					
+					describe('~index', function () {
+						
+						afterEach(function () {
+							collection.empty({destroy: true});
+						});
+						
+						it ('should default to adding models to the end if not set explicitly',
+							function () {
+							var model = new Model();
+							
+							collection.add([{}, {}, {}]);
+							
+							// check to make sure that it will add it at the end automatically
+							collection.add(model);
+							expect(collection.at(3)).to.eql(model);
+							model.destroy();
+						});
+						
+						it ('should add a model at the given valid index', function () {
+							var model = new Model();
+							
+							collection.add([{}, {}, {}]);
+							
+							// add it at a different index to see if it updates the indices
+							// properly
+							collection.add(model, {index: 1});
+							expect(collection.length).to.equal(4);
+							expect(collection.at(1)).to.eql(model);
+							model.destroy();
+						});
+						
+						it ('should add models at the given valid index', function () {
+							var model1 = new Model(),
+								model2 = new Model(),
+								ary = [model1, {}, {}, model2];
+								
+							collection.add([{}, {}, {}]);
+							
+							// splice these models into the collection at the requested index
+							collection.add(ary, {index: 0});
+							expect(collection.length).to.equal(7);
+							expect(collection.at(0)).to.eql(model1);
+							expect(collection.at(3)).to.eql(model2);
+							model1.destroy();
+							model2.destroy();
+						});
+						
+					});
+					
+					describe('~modelOptions', function () {
+						
+						afterEach(function () {
+							collection.empty({destroy: true});
+						});
+						
+						it ('should pass these options to the enyo.Model constructor when ' +
+							'create is true', function () {
+							var spy = sinon.spy(Model.prototype, 'parse');
+							
+							collection.add({}, {modelOptions: {parse: true}});
+							
+							expect(spy.callCount).to.equal(1);
+							spy.restore();
+						});
+						
+					});
+					
+				});
+				
+			});
+			
 		});
 		
 		describe('#remove', function () {
+			
+			it ('should respond to the method remove', function () {
+				expect(proto).to.itself.respondTo('remove');
+			});
+			
+			describe('params', function () {
+				
+				var collection;
+				
+				before(function () {
+					collection = new Collection();
+				});
+				
+				after(function () {
+					collection.destroy({destroy: true});
+				});
+				
+				describe('@models', function () {
+					
+					before(function () {
+						collection.add([
+							{},
+							{},
+							{}
+						]);
+					});
+					
+					it ('should accept a model instance', function () {
+						collection.remove(collection.at(0));
+						expect(collection.length).to.equal(2);
+					});
+					
+					it ('should accept an array of model instances', function () {
+						collection.remove(collection.models);
+						expect(collection.length).to.equal(0);
+					});
+					
+				});
+				
+				describe('@opts', function () {
+					
+					
+					
+				});
+				
+			});
 			
 		});
 		
