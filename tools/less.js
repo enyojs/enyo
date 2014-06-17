616624995ec55165396e3f1455a872a60033c359
Jason Robitaille <robitaille88@gmail.com>
2014-06-16 20:32:42 -0500

    Allow for better location detection of less, when enyo is not in root directory

--- tools/less.js
+++ tools/less.js
@@ -8,11 +8,14 @@
 		less.environment = "production";
 	}
 	window.less = less;
-	var path = "enyo/tools/minifier/node_modules/less/dist/less-1.3.3.min.js";
-	var self = document.getElementById("less-loader");
-	if(self) {
-		// optionally update path relative to this script for extra precision
-		path = self.src.replace("enyo/tools/less.js", path);
+	var path = "enyo/tools/minifier/node_modules/less/dist/less-1.7.0.min.js";
+	var scripts = document.getElementsByTagName("script");
+	for(var i=0; i<scripts.length; i++) {
+		if(scripts[i].src.indexOf("enyo/tools/less.js")>-1) {
+			// update path relative to this script for extra precision
+			path = self.src.replace("enyo/tools/less.js", path);
+			break;
+		}
 	}
 	var script = document.createElement('script');
 	script.src = path;
