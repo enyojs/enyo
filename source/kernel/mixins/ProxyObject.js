397f409efc59ad5db12f365be18770676fcbe84e
Cole Davis <cole.davis@lge.com>
2014-06-18 17:12:14 -0700

    remove notion of getLocal/setLocal and ensure previously expected behavior continues
    Enyo-DCO-1.1-Signed-Off-By: Cole Davis (cole.davis@lge.com)

--- source/kernel/mixins/ProxyObject.js
+++ source/kernel/mixins/ProxyObject.js
@@ -1,71 +1,88 @@
-(function (enyo) {
+(function (enyo, scope) {
 	
 	/**
-		This mixin is designed to abstract interaction of the _get()_/_set()_ methods. It allows
-		a specific property to be named as the _proxy object_ {@link #proxyObjectKey}. The _proxy object_
-		is the target object on which calls to _get()_/_set()_ will be made. It also adds convenience
-		methods _getLocal()_/_setLocal()_ that will operate on the base object instance. There are usually
-		limitations involved with using this mixin such as limited {@link Binding} or specific {@link Observer}
-		support.
+		A mixin designed to abstract interaction of the {@link enyo.Object#get} and
+		{@link enyo.Object#set} methods. It does not need to be applied to a subclass of
+		{@link enyo.Object}.
 	
-		@public
+		@abstract
 		@mixin enyo.ProxyObject
+		@public
 	*/
 	enyo.ProxyObject = {
-		name: "ProxyObject",
 		
 		/**
+			@private
+		*/
+		name: 'ProxyObject',
+		
+		/**
+			The property designated as the object from which to proxy actions.
+			
+			@type {string}
+			@default 'data'
 			@public
 		*/
-		proxyObjectKey: "data",
+		proxyObjectKey: 'data',
 		
 		/**
-			@public
+			The overloaded [getter]{@link enyo.Object#get}. Acccepts the same parameters and
+			attempts to call the same method on the [proxy]{@link enyo.ProxyObject~proxyObjectKey}.
+			If it cannot find a [proxy]{@link enyo.ProxyObject~proxyObjectKey} it will return
+			`undefined`. {@see enyo.Object#get} and {@see enyo.getPath}.
+			
+			@param {string} path The path from which to retrieve a value.
+			@returns {*} The value for the given path or `undefined` if the path could not be
+				completely resolved.
 			@method
+			@public
 		*/
 		get: function (path) {
-			var key = this.proxyObjectKey
-				, proxy = this[key];
+			var key = this.proxyObjectKey,
+				proxy = this[key];
 			
-			return proxy? proxy.get? proxy.get.apply(proxy, arguments): enyo.getPath.call(proxy, path): undefined;
+			if (proxy) {
+				if (proxy.get) return proxy.get.apply(proxy, arguments);
+				else return enyo.getPath.call(proxy, path);
+			}
+			
+			return undefined;
 		},
 		
 		/**
-			@public
+			The overloaded [setter]{@link enyo.Object#set}. Accepts the same parameters and
+			attempts to call the same method on the [proxy]{@link enyo.ProxyObject~proxyObjectKey}.
+			If it cannot find the proxy it returns the callee for chaining.
+			
+			@param {string} path The path for which to set the given value.
+			@param {*} is The value to set.
+			@param {object} [opts] An options hash.
+			@returns {*} The callee for chaining.
 			@method
+			@public
 		*/
 		set: function (path, is, opts) {
-			var key = this.proxyObjectKey
-				, proxy = this[key]
-				, was, force;
-			// for backwards compatibility
-			force = enyo.isObject(opts)? opts.force: opts;
+			var key = this.proxyObjectKey,
+				proxy = this[key],
+				was,
+				force;
+			
+			force = typeof opts == 'object' ? opts.force : opts;
 			
 			if (proxy) {
 				was = this.get(path);
-				proxy.set? proxy.set.apply(proxy, arguments): enyo.setPath.apply(proxy, arguments);
+				if (proxy.set) proxy.set.apply(proxy, arguments);
+				else enyo.setPath.apply(proxy, arguments);
 				
-				if (this.notify && (force || was !== is || (opts && opts.compare && opts.compare(was, is)))) this.notify(path, was, is);
+				if (this.notify && (
+					force ||
+					was !== is ||
+					(opts && opts.compare && opts.compare(was, is))
+				)) this.notify(path, was, is);
 			}
 			
 			return this;
-		},
-		
-		/**
-			@public
-			@method
-		*/
-		getLocal: function (path) {
-			return enyo.getPath.call(this, path);
-		},
-		
-		/**
-			@public
-			@method
-		*/
-		setLocal: function (path, is, opts) {
-			return enyo.setPath.call(this, path, is, opts);
 		}
 	};
 	
-})(enyo);
+})(enyo, this);
