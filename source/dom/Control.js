e21b84a0d486b1a9275387c0afc53ba2637cbb13
David.Um <david.um@lge.com>
2014-06-27 15:18:24 +0900

    BHV-5137 Render this control which has enabled renderOnShow
    If renderOnShow is true then we will delay its rendering until there is a request to show it.
    
    Enyo-DCO-1.1-Signed-off-by: David Um david.um@lge.com

--- source/dom/Control.js
+++ source/dom/Control.js
@@ -541,8 +541,12 @@
 			
 			// if we are changing from not showing to showing we attempt to find whatever
 			// our last known value for display was or use the default
-			if (!was) this.applyStyle('display', this._display || '');
-			
+			if (!was) {
+				this.applyStyle('display', this._display || '');
+				// If renderOnShow is true and canGenerate is false then
+				// develper intended to make this control un-rendered until got request to show
+				if (this.renderOnShow && !this.canGenerate) this.render();
+			}			
 			// if we are supposed to be hiding the control then we need to cache our current
 			// display state
 			else if (!this.showing) {
