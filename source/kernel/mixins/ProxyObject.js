9c2efc9a1cea7b7bd694065be56052c7da20fe60
Cole Davis <cole.davis@lge.com>
2014-07-01 10:13:48 -0700

    docs for ProxyObjectSupport
    Enyo-DCO-1.1-Signed-Off-By: Cole Davis (cole.davis@lge.com)

--- source/kernel/mixins/ProxyObject.js
+++ source/kernel/mixins/ProxyObject.js
@@ -1,41 +1,40 @@
 (function (enyo, scope) {
 	
 	/**
-		A mixin designed to abstract interaction of the {@link enyo.Object#get} and
-		{@link enyo.Object#set} methods. It does not need to be applied to a subclass of
-		{@link enyo.Object}.
-	
-		@abstract
-		@mixin enyo.ProxyObject
-		@public
+	* A mixin designed to abstract interaction of the {@link enyo.Object#get} and
+	* {@link enyo.Object#set} methods. It does not need to be applied to a
+	* [subkind]{@link external:subkind} of {@link enyo.Object}.
+	* 
+	* @mixin enyo.ProxyObject
+	* @public
 	*/
 	enyo.ProxyObject = {
 		
 		/**
-			@private
+		* @private
 		*/
 		name: 'ProxyObject',
 		
 		/**
-			The property designated as the object from which to proxy actions.
-			
-			@type {string}
-			@default 'data'
-			@public
+		* The property designated as the object from which to proxy actions.
+		* 
+		* @type {String}
+		* @default 'data'
+		* @public
 		*/
 		proxyObjectKey: 'data',
 		
 		/**
-			The overloaded [getter]{@link enyo.Object#get}. Acccepts the same parameters and
-			attempts to call the same method on the [proxy]{@link enyo.ProxyObject~proxyObjectKey}.
-			If it cannot find a [proxy]{@link enyo.ProxyObject~proxyObjectKey} it will return
-			`undefined`. {@see enyo.Object#get} and {@see enyo.getPath}.
-			
-			@param {string} path The path from which to retrieve a value.
-			@returns {*} The value for the given path or `undefined` if the path could not be
-				completely resolved.
-			@method
-			@public
+		* The overloaded [getter]{@link enyo.Object#get}. Acccepts the same parameters and
+		* attempts to call the same method on the [proxy]{@link enyo.ProxyObject.proxyObjectKey}.
+		* If it cannot find a [proxy]{@link enyo.ProxyObject.proxyObjectKey} it will return
+		* `undefined`. {@see enyo.Object#get} and {@see enyo.getPath}.
+		* 
+		* @param {String} path The path from which to retrieve a value.
+		* @returns {this} The value for the given path or `undefined` if the path could not be
+		* 	completely resolved.
+		* @method
+		* @public
 		*/
 		get: function (path) {
 			var key = this.proxyObjectKey,
@@ -50,16 +49,16 @@
 		},
 		
 		/**
-			The overloaded [setter]{@link enyo.Object#set}. Accepts the same parameters and
-			attempts to call the same method on the [proxy]{@link enyo.ProxyObject~proxyObjectKey}.
-			If it cannot find the proxy it returns the callee for chaining.
-			
-			@param {string} path The path for which to set the given value.
-			@param {*} is The value to set.
-			@param {object} [opts] An options hash.
-			@returns {*} The callee for chaining.
-			@method
-			@public
+		* The overloaded [setter]{@link enyo.Object#set}. Accepts the same parameters and
+		* attempts to call the same method on the [proxy]{@link enyo.ProxyObject~proxyObjectKey}.
+		* If it cannot find the proxy it returns the callee for chaining.
+		* 
+		* @param {String} path The path for which to set the given value.
+		* @param {*} is The value to set.
+		* @param {Object} [opts] An options hash.
+		* @returns {this} The callee for chaining.
+		* @method
+		* @public
 		*/
 		set: function (path, is, opts) {
 			var key = this.proxyObjectKey,
