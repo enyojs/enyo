feb30228218b5d2f1a188bdc53bc847022a1eb57
Blake Stephens <blake.stephens@lge.com>
2014-07-08 16:30:19 -0700

    Quote replacement on platform.js

--- source/dom/platform.js
+++ source/dom/platform.js
@@ -23,11 +23,11 @@
 	* ```javascript
 	* // android 2 does not have 3d css
 	* if (enyo.platform.android < 3) {
-	* 	t = "translate(30px, 50px)";
+	* 	t = 'translate(30px, 50px)';
 	* } else {
-	* 	t = "translate3d(30px, 50px, 0)";
+	* 	t = 'translate3d(30px, 50px, 0)';
 	* }
-	* this.applyStyle("-webkit-transform", t);
+	* this.applyStyle('-webkit-transform', t);
 	* ```
 	*
 	* @name enyo.platform
@@ -35,9 +35,9 @@
 	enyo.platform = 
 		/** @lends enyo.platform */ {
 		//* True if the platform has native single-finger events
-		touch: Boolean(("ontouchstart" in window) || window.navigator.msMaxTouchPoints),
+		touch: Boolean(('ontouchstart' in window) || window.navigator.msMaxTouchPoints),
 		//* True if the platform has native double-finger events
-		gesture: Boolean(("ongesturestart" in window) || window.navigator.msMaxTouchPoints)
+		gesture: Boolean(('ongesturestart' in window) || window.navigator.msMaxTouchPoints)
 	};
 
 	/**
@@ -47,45 +47,45 @@
 	var ep = enyo.platform;
 	var platforms = [
 		// Android 4+ using Chrome
-		{platform: "androidChrome", regex: /Android .* Chrome\/(\d+)[.\d]+/},
+		{platform: 'androidChrome', regex: /Android .* Chrome\/(\d+)[.\d]+/},
 		// Android 2 - 4
-		{platform: "android", regex: /Android (\d+)/},
+		{platform: 'android', regex: /Android (\d+)/},
 		// Kindle Fire
 		// Force version to 2, (desktop mode does not list android version)
-		{platform: "android", regex: /Silk\/1./, forceVersion: 2, extra: {silk: 1}},
+		{platform: 'android', regex: /Silk\/1./, forceVersion: 2, extra: {silk: 1}},
 		// Kindle Fire HD (Silk versions 2 or 3)
 		// Force version to 4
-		{platform: "android", regex: /Silk\/2./, forceVersion: 4, extra: {silk: 2}},
-		{platform: "android", regex: /Silk\/3./, forceVersion: 4, extra: {silk: 3}},
+		{platform: 'android', regex: /Silk\/2./, forceVersion: 4, extra: {silk: 2}},
+		{platform: 'android', regex: /Silk\/3./, forceVersion: 4, extra: {silk: 3}},
 		// Windows Phone 7 - 8
-		{platform: "windowsPhone", regex: /Windows Phone (?:OS )?(\d+)[.\d]+/},
+		{platform: 'windowsPhone', regex: /Windows Phone (?:OS )?(\d+)[.\d]+/},
 		// IE 8 - 10
-		{platform: "ie", regex: /MSIE (\d+)/},
+		{platform: 'ie', regex: /MSIE (\d+)/},
 		// IE 11
-		{platform: "ie", regex: /Trident\/.*; rv:(\d+)/},
+		{platform: 'ie', regex: /Trident\/.*; rv:(\d+)/},
 		// iOS 3 - 5
 		// Apple likes to make this complicated
-		{platform: "ios", regex: /iP(?:hone|ad;(?: U;)? CPU) OS (\d+)/},
+		{platform: 'ios', regex: /iP(?:hone|ad;(?: U;)? CPU) OS (\d+)/},
 		// webOS 1 - 3
-		{platform: "webos", regex: /(?:web|hpw)OS\/(\d+)/},
+		{platform: 'webos', regex: /(?:web|hpw)OS\/(\d+)/},
 		// webOS 4 / OpenWebOS
-		{platform: "webos", regex: /WebAppManager|Isis/, forceVersion: 4},
+		{platform: 'webos', regex: /WebAppManager|Isis/, forceVersion: 4},
 		// desktop Safari
-		{platform: "safari", regex: /Version\/(\d+)[.\d]+\s+Safari/},
+		{platform: 'safari', regex: /Version\/(\d+)[.\d]+\s+Safari/},
 		// desktop Chrome
-		{platform: "chrome", regex: /Chrome\/(\d+)[.\d]+/},
+		{platform: 'chrome', regex: /Chrome\/(\d+)[.\d]+/},
 		// Firefox on Android
-		{platform: "androidFirefox", regex: /Android;.*Firefox\/(\d+)/},
+		{platform: 'androidFirefox', regex: /Android;.*Firefox\/(\d+)/},
 		// FirefoxOS
-		{platform: "firefoxOS", regex: /Mobile;.*Firefox\/(\d+)/},
+		{platform: 'firefoxOS', regex: /Mobile;.*Firefox\/(\d+)/},
 		// desktop Firefox
-		{platform: "firefox", regex: /Firefox\/(\d+)/},
+		{platform: 'firefox', regex: /Firefox\/(\d+)/},
 		// Blackberry Playbook
-		{platform: "blackberry", regex: /PlayBook/i, forceVersion: 2},
+		{platform: 'blackberry', regex: /PlayBook/i, forceVersion: 2},
 		// Blackberry 10+
-		{platform: "blackberry", regex: /BB1\d;.*Version\/(\d+\.\d+)/},
+		{platform: 'blackberry', regex: /BB1\d;.*Version\/(\d+\.\d+)/},
 		// Tizen
-		{platform: "tizen", regex: /Tizen (\d+)/}
+		{platform: 'tizen', regex: /Tizen (\d+)/}
 	];
 	for (var i = 0, p, m, v; (p = platforms[i]); i++) {
 		m = p.regex.exec(ua);
