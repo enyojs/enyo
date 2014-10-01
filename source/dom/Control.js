48ff45bd61a241c12224621989836a1826bbdcd5
Cole Davis <cole.davis@lge.com>
2014-10-01 16:24:58 -0700

    can't use teardownRender as a reset point because it is called at the beginning of each 'render'
    Enyo-DCO-1.1-Signed-Off-By: Cole Davis (cole.davis@lge.com)

--- source/dom/Control.js
+++ source/dom/Control.js
@@ -214,10 +214,8 @@
 		* to be `false`. Arbitrarily modifying the values of these properties prior to its initial
 		* render may have unexpected results.
 		*
-		* Also note that if `renderOnShow` is `true` and the control has its
-		* [teardownRender]{@link enyo.Control#teardownRender} method called, these properties will
-		* be set to `false` again to preserve the original behavior. If the desire is only to delay
-		* render the initial time set `renderOnShow` to `false` post-render.
+		* Once a control has been shown/rendered with `renderOnShow` `true` the behavior will not
+		* be used again.
 		*
 		* @type {Boolean}
 		* @default false
@@ -833,6 +831,11 @@
 		* @private
 		*/
 		showingChangedHandler: function (sender, event) {
+			
+			if (!this.showing && event.showing && this.renderOnShow && !this.generated) {
+				this.set('showing', true);
+			}
+			
 			return sender === this ? false : !this.showing;
 		},
 
@@ -1070,13 +1073,6 @@
 			}
 
 			delegate.teardownRender(this);
-			
-			// if the original state was set with renderOnShow true then we need to reset these
-			// values as well to coordinate the original intent
-			if (this.renderOnShow) {
-				this.set('showing', false);
-				this.set('canGenerate', false);
-			}
 		},
 
 		/**
