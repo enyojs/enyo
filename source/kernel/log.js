cbf26ea3d3998c8f25c1b4e6fcc116b0fae167ff
Jim Tang <jim.tang@lge.com>
2014-10-29 15:57:36 -0700

    JSDoc cleanup. Enyo-DCO-1.1-Signed-Off-By: Jim Tang (jim.tang@lge.com)

--- source/kernel/log.js
+++ source/kernel/log.js
@@ -11,7 +11,7 @@
 		/**
 		* The log level to use. Can be a value from -1 to 99, where -1 disables all
 		* logging, 0 is 'error', 10 is 'warn', and 20 is 'log'. It is preferred that
-		* this value be set using the [enyo.setLogLevel()]{@link enyo.setLogLevel}
+		* this value be set using the [enyo.setLogLevel()]{@link enyo~setLogLevel}
 		* method.
 		*
 		* @type {Number}
