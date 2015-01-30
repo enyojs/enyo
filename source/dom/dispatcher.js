995933072e59a4d289173b88a2f35d4d8aebe919
Kunmyon Choi <kunmyon.choi@lge.com>
2015-01-30 15:39:58 +0900

    ENYO-923: Stop marquee when window throw blur event
    Add 'focus' indo windowEvents for ENYO-924 issue
    
    DCO-1.1-Signed-Off-By: Kunmyon Choi kunmyon.choi@lge.com

--- source/dom/dispatcher.js
+++ source/dom/dispatcher.js
@@ -42,7 +42,7 @@
 		*
 		* @private
 		*/
-		windowEvents: ["resize", "load", "unload", "message", "hashchange", "popstate", "blur"],
+		windowEvents: ["resize", "load", "unload", "message", "hashchange", "popstate", "focus", "blur"],
 
 		/**
 		* Feature plugins (aka filters)
