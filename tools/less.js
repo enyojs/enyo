2e731fbf5ce8a3be45758e488d1068a19a8b585b
Aaron Tam <aaron.tam@lge.com>
2015-02-04 17:04:38 -0800

    Revert "Merge pull request #1026 from enyojs/ENYO-952-aarontam"
    This reverts commit cda38565f23e7f786364718d6a0ceca868af06ce, reversing
    changes made to 02db5f8340a6d1ab1ca3f52fb78b09f6659fe338.

--- tools/less.js
+++ tools/less.js
@@ -9,7 +9,18 @@
 	}
 	window.less = less;
 	var script = document.createElement('script');
-	script.src = "enyo/tools/minifier/node_modules/less/dist/less-1.7.0.min.js";
+	script.src = "enyo/tools/minifier/node_modules/less/dist/less-1.7.0.ri.min.js";
 	script.charset = "utf-8";
+	document.getElementsByTagName('head')[0].appendChild(script);
+
+	script = document.createElement('script');
+	script.src = "enyo/tools/minifier/node_modules/less-plugin-resolution-independence/lib/resolution-independence.js";
+	script.charset = "utf-8";
+	script.onload = function () {
+		var less = window.less || {};
+		var ri = new enyoLessRiPlugin();
+		less.plugins = [ri];
+		window.less = less;
+	}
 	document.getElementsByTagName('head')[0].appendChild(script);
 })();
