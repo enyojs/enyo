4d115ff8ec737696edda6f38e00f60d431b20015
Ryan Duffy <ryan.duffy@lge.com>
2015-03-06 15:07:31 -0600

    add support in less bootstrap to select ri version of LESS
    Issue: ENYO-1140
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- tools/less.js
+++ tools/less.js
@@ -9,13 +9,28 @@
 	}
 	window.less = less;
 
+	function script (src) {
+		var node = document.createElement('script');
+		node.src = src;
+		node.charset = 'utf-8';
+		document.getElementsByTagName('head')[0].appendChild(node);
+
+		return node;
+	}
+
 	// find the relative path to the less script by finding this script tag and using its src
 	var thisScript = document.querySelector('script[src $= \'enyo/tools/less.js\']'),
 		src = thisScript ? thisScript.getAttribute('src') : null,
 		basePath = src ? src.substring(0, src.length - 7) : 'enyo/tools/',
-		script = document.createElement('script');
+		useRI = thisScript && thisScript.hasAttribute('ri'),
+		lessJs = useRI ? 'less-1.7.0.ri.min.js' : 'less-1.7.0.min.js';
 
-	script.src = basePath + 'minifier/node_modules/less/dist/less-1.7.0.min.js';
-	script.charset = 'utf-8';
-	document.getElementsByTagName('head')[0].appendChild(script);
+	script(basePath + 'minifier/node_modules/less/dist/' + lessJs);
+	if (useRI) {
+		var riSrc = basePath + 'minifier/node_modules/less-plugin-resolution-independence/lib/resolution-independence.js'; 
+		script(riSrc).onload = function () {
+			var ri = new enyoLessRiPlugin();
+			less.plugins = [ri];
+		};
+	}
 })();
