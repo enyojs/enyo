81fe1bffb71ae5f3dec13b2f3a35e507d1dc7930
Ryan Duffy <ryan.duffy@lge.com>
2015-04-01 10:51:00 -0500

    add more variations to Image sample
    Issue: ENYO-510
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- samples/ImageSample.js
+++ samples/ImageSample.js
@@ -1,12 +1,20 @@
 enyo.kind({
 	name: "enyo.sample.ImageSample",
+	kind: "Scroller",
+	horizontal: "hidden",
 	classes: "image-sample",
 	components: [
 		{content: "Image", classes: "section"},
 		{kind: "enyo.Image", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
-		{content: "Background Image", classes: "section"},
+		{content: "Sizing Constrain", classes: "section"},
 		{kind: "enyo.Image", style: "width:200px;height:75px;background-color:gray", sizing: "constrain", position: "left top", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
-		{content: "Placeholder Image", classes: "section"},
-		{kind: "enyo.Image", src: enyo.Image.placeholder, alt: "Placeholder Image", classes: "placeholder"}
+		{content: "Sizing Cover", classes: "section"},
+		{kind: "enyo.Image", style: "width:200px;height:75px;background-color:gray", sizing: "cover", position: "left top", src: "http://enyojs.com/img/enyo-logo.png", alt: "Enyo Logo"},
+		{content: "Only Placeholder Image", classes: "section"},
+		{kind: "enyo.Image", placeholder: enyo.Image.placeholder, alt: "Placeholder Image", classes: "placeholder"},
+		{content: "Placeholder and Src, Sizing Constrain", classes: "section"},
+		{kind: "enyo.Image", src: "http://enyojs.com/img/enyo-logo.png", sizing: 'constrain', placeholder: enyo.Image.placeholder, alt: "Placeholder Image", classes: "placeholder"},
+		{content: "Placeholder and Src, No Sizing", classes: "section"},
+		{kind: "enyo.Image", src: "http://enyojs.com/img/enyo-logo.png", placeholder: enyo.Image.placeholder, alt: "Placeholder Image", classes: "placeholder"}
 	]
 });
