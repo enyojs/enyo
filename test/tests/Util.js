70ce47bf1ee760e0bd4276467f064c9bfc37cf9f
Cole Davis <cole.davis@lge.com>
2015-04-06 16:57:33 -0700

    add back utils mocha test

--- test/tests/Util.js
+++ test/tests/Util.js
@@ -1,7 +1,10 @@
+var
+	utils = require('../../lib/utils');
+
 describe('Utilities', function () {
 	
-	// tests for the enyo.mixin functionality
-	describe('enyo.mixin', function () {
+	// tests for the utils.mixin functionality
+	describe('utils.mixin', function () {
 		
 		var src1 = {key1: 'value1', key2: 'value2'},
 			src2 = {key3: 'value3', key4: 'value4'};
@@ -11,7 +14,7 @@ describe('Utilities', function () {
 			
 			var ret = {};
 			
-			enyo.mixin(ret, src1);
+			utils.mixin(ret, src1);
 			
 			// ret should be the exact same as teh src1 object but not be the same reference
 			expect(ret).to.deep.equal(src1);
@@ -19,7 +22,7 @@ describe('Utilities', function () {
 		});
 		
 		it ('should create a new object if first parameter is null or undefined', function () {
-			var ret = enyo.mixin(null, src1);
+			var ret = utils.mixin(null, src1);
 			
 			// ret should be the exact same as the src1 object but not be the same reference
 			expect(ret).to.deep.equal(src1);
@@ -29,7 +32,7 @@ describe('Utilities', function () {
 		it ('should copy the keys of all objects in an array as the second parameter to the first',
 			function () {
 			
-			var ret = enyo.mixin({}, [src1, src2]);
+			var ret = utils.mixin({}, [src1, src2]);
 			
 			expect(ret).to.have.property('key1', 'value1');
 			expect(ret).to.have.property('key2', 'value2');
@@ -40,7 +43,7 @@ describe('Utilities', function () {
 		it ('should copy the keys of all objects in an array as the first parameter to a new ' +
 			'object', function () {
 		
-			var ret = enyo.mixin([src1, src2]);
+			var ret = utils.mixin([src1, src2]);
 	
 			expect(ret).to.have.property('key1', 'value1');
 			expect(ret).to.have.property('key2', 'value2');
@@ -51,7 +54,7 @@ describe('Utilities', function () {
 		it ('should not copy keys that are already set on the return object when the ignore ' +
 			'option flag is set', function () {
 			
-			var ret = enyo.mixin([src1, src2, {key1: 'WRONG VALUE'}], {ignore: true});
+			var ret = utils.mixin([src1, src2, {key1: 'WRONG VALUE'}], {ignore: true});
 			
 			expect(ret).to.have.property('key1', 'value1');
 			expect(ret).to.have.property('key2', 'value2');
@@ -62,7 +65,7 @@ describe('Utilities', function () {
 		it ('should not copy undefined keys from source objects if the exists option flag is set',
 			function () {
 			
-			var ret = enyo.mixin([src1, src2, {key5: undefined}], {exists: true});
+			var ret = utils.mixin([src1, src2, {key5: undefined}], {exists: true});
 		
 			expect(ret).to.have.property('key1', 'value1');
 			expect(ret).to.have.property('key2', 'value2');
@@ -82,7 +85,7 @@ describe('Utilities', function () {
 				return Number(key.charAt(3)) >= 3;
 			};
 			
-			enyo.mixin(ret, [src1, src2, {key5: 'value5'}], {filter: fn});
+			utils.mixin(ret, [src1, src2, {key5: 'value5'}], {filter: fn});
 			
 			expect(ret).to.have.property('key3', 'value3');
 			expect(ret).to.have.property('key4', 'value4');
