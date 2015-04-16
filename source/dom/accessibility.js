c11e51cbec7b920d9e22fb779e115ba13403ef5d
jaewon98.jang <jaewon98.jang@lgepartner.com>
2015-04-16 17:12:04 +0900

    ENYO-1188: Accessibility: Apply "aria-label" to all enyo control which have a content
    When content of control is set or chagned, aria-label should be also set
    and changed. So I added the code of setting aria-label in contentChanged
    function.
    In addition, it needs the tabindex attribute to be focusable because
    screen reader can read aria-label when dom is focused. And if dom has
    tabindex attribute default outline is shown when it focused, so remove
    the outline in case dom has tabindex attribute.
    
    https://jira2.lgsvl.com/browse/ENYO-1188
    Enyo-DCO-1.1-Signed-off-by: Jaewon Jang jaewon98.jang@lgepartner.com

---
+++ source/dom/accessibility.js
@@ -0,0 +1,24 @@
+(function (enyo, scope) {
+
+	if (enyo.options.accessibility) {
+		enyo.Control.extend(
+			/** @lends enyo.Control.prototype */ {
+
+			/**
+			* @private
+			*/
+			contentChanged: enyo.inherit(function (sup) {
+				return function (control) {
+					sup.apply(this, arguments);
+
+					// Accessibility : Set aria-label to current content 
+					// when content changed.
+					if (this.content) {
+						this.setAttribute('tabindex', 0);
+						this.setAttribute('aria-label', this.content);
+					}
+				};
+			})
+		})
+	}
+})(enyo, this);
