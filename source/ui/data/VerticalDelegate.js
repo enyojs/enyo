1c5224b9423b3ee486c312cf587b7f2a3ffb55e9
david.um <david.um@lge.com>
2015-04-17 16:37:01 +0900

    Enyo-966 Separate  updating page part from generatePage()
    For more flexible usage, separate some logic into individual method.
    
    Enyo-DCO-1.1-Signed-off-by: David Um david.um@lge.com

--- source/ui/data/VerticalDelegate.js
+++ source/ui/data/VerticalDelegate.js
@@ -135,36 +135,19 @@
 			// want to do any more initialization
 			if (list.collection && list.collection.length) { this.reset(list); }
 		},
+
 		/**
-		* Generates the markup for the page content.
+		* Update page with model
 		*
 		* @private
 		*/
-		generatePage: function (list, page, index) {
-			// Temporarily add logging code to make it easier for
-			// QA and others to detect and report page-index issues
-			if (index < 0) {
-				enyo.warn('Invalid page index: ' + index);
-			}
-			// in case it hasn't been set we ensure it is marked correctly
-			page.index  = index;
-				// the collection of data with records to use
-			var data    = list.collection,
-				// the metrics for the entire list
-				metrics = list.metrics,
-				// controls per page
-				perPage = this.controlsPerPage(list),
-				// placeholder for the control we're going to update
+		updatePage: function(list, page) {
+			var data = list.collection,
+			// placeholder for the control we're going to update
 				view;
-
-			// the first index for this generated page
-			page.start  = perPage * index;
-			// the last index for this generated page
-			page.end    = Math.min((data.length - 1), (page.start + perPage) - 1);
-
 			// if generating a control we need to use the correct page as the control parent
 			list.controlParent = page;
-			for (var i=page.start; i <= page.end && i < data.length; ++i) {
+			for (var i = page.start; i <= page.end && i < data.length; ++i) {
 				view = (page.children[i - page.start] || list.createComponent({}));
 				// disable notifications until all properties to be updated
 				// have been
@@ -186,6 +169,35 @@
 			// update the entire page at once - this removes old nodes and updates
 			// to the correct ones
 			page.render();
+		},
+
+		/**
+		* Generates the markup for the page content.
+		*
+		* @private
+		*/
+		generatePage: function (list, page, index) {
+			// Temporarily add logging code to make it easier for
+			// QA and others to detect and report page-index issues
+			if (index < 0) {
+				enyo.warn('Invalid page index: ' + index);
+			}
+			// in case it hasn't been set we ensure it is marked correctly
+			page.index  = index;
+				// the collection of data with records to use
+			var data    = list.collection,
+				// the metrics for the entire list
+				metrics = list.metrics,
+				// controls per page
+				perPage = this.controlsPerPage(list);
+
+			// the first index for this generated page
+			page.start  = perPage * index;
+			// the last index for this generated page
+			page.end    = Math.min((data.length - 1), (page.start + perPage) - 1);
+
+			this.updatePage(list, page);
+
 			// now to update the metrics
 			metrics        = metrics.pages[index] || (metrics.pages[index] = {});
 			metrics.height = this.pageHeight(list, page);
