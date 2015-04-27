23756690e4a22914a7eaa97a1fac5c481465f022
Ryan Duffy <ryan.duffy@lge.com>
2015-04-27 14:03:40 -0500

    prototype enyo/global
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 'use strict';
+var
+	gl = require('./lib/global');
 
 exports = module.exports = require('./lib/options');
 exports.version = '2.6.0-pre';
+exports.include = gl.include ? gl.include.bind(gl, 'enyo') : function () {};
