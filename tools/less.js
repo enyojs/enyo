86f071e892ef042e44e6b03d6ca21228fe2754b8
Aaron Tam <aaron.tam@lge.com>
2015-06-10 10:17:44 -0700

    ENYO-1795: Enable configuration of resolution-independence plugin in browser.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- tools/less.js
+++ tools/less.js
@@ -1,24 +1,40 @@
 // Client-side less.js configurator / loader
 (function() {
+	// parse script for options
+	var scripts = document.getElementsByTagName('script'),
+		index = scripts.length - 1,
+		riParams = scripts[index].getAttribute('ri-params'),
+		opts;
+
+	if (riParams) {
+		opts = {};
+		riParams.replace(
+			new RegExp('([^,=&]+)(=([^,]*))?', 'g'),
+			function($0, $1, $2, $3) {
+				opts[$1] = $3;
+			}
+		);
+	}
+
 	var less = window.less || {};
 	if (less.relativeUrls === undefined) {
 		less.relativeUrls = true;
 	}
 	if (less.environment === undefined) {
-		less.environment = "production";
+		less.environment = 'production';
 	}
 	window.less = less;
 	var script = document.createElement('script');
-	script.src = "enyo/tools/minifier/node_modules/less/dist/less-1.7.0.min.js";
-	script.charset = "utf-8";
+	script.src = 'enyo/tools/minifier/node_modules/less/dist/less-1.7.0.min.js';
+	script.charset = 'utf-8';
 	document.getElementsByTagName('head')[0].appendChild(script);
 
 	script = document.createElement('script');
-	script.src = "enyo/tools/minifier/node_modules/less-plugin-resolution-independence/lib/resolution-independence.js";
-	script.charset = "utf-8";
+	script.src = 'enyo/tools/minifier/node_modules/less-plugin-resolution-independence/lib/resolution-independence.js';
+	script.charset = 'utf-8';
 	script.onload = function () {
 		var less = window.less || {};
-		var ri = new enyoLessRiPlugin();
+		var ri = new enyoLessRiPlugin(opts);
 		less.plugins = [ri];
 		window.less = less;
 	}
