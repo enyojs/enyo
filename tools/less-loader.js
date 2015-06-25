56eafd76e968c9b7da956a2a8548ff0418c6bf4f
Derek Anderson <derek.anderson@lge.com>
2015-06-25 13:16:03 -0700

    new less plugin to handle normalizing prefix use in less

--- tools/less-loader.js
+++ tools/less-loader.js
@@ -1,6 +1,7 @@
 // Client-side less.js configurator / loader
 (function() {
 	var less = window.less || {};
+	less.plugins = [];
 	if (less.relativeUrls === undefined) {
 		less.relativeUrls = true;
 	}
@@ -17,10 +18,18 @@
 	script.src = "enyo/tools/minifier/node_modules/less-plugin-resolution-independence/lib/resolution-independence.js";
 	script.charset = "utf-8";
 	script.onload = function () {
-		var less = window.less || {};
 		var ri = new enyoLessRiPlugin();
-		less.plugins = [ri];
-		window.less = less;
+		less.plugins.push(ri);
+	}
+	document.getElementsByTagName('head')[0].appendChild(script);	
+	
+	script = document.createElement('script');
+	script.src = "enyo/tools/minifier/node_modules/less-plugin-prefixly/lib/prefixly.js";
+	script.charset = "utf-8";
+	script.onload = function () {
+		var pfx = new enyoLessPrefixlyPlugin();
+		less.plugins.push(pfx);
 	}
 	document.getElementsByTagName('head')[0].appendChild(script);
+	
 })();
