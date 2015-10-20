4dc24349606613e8ae88c48e25025a3cf2b276ee
Ryan Duffy <ryan.duffy@lge.com>
2015-10-20 12:08:08 -0500

    allow customization of scrim in ScrimSupport
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- src/ViewManager/ScrimSupport.js
+++ src/ViewManager/ScrimSupport.js
@@ -1,16 +1,24 @@
 var
-	kind = require('../kind');
+	kind = require('../kind'),
+	utils = require('enyo/utils');
 
 module.exports = {
 	initComponents: kind.inherit(function (sup) {
 		return function () {
+			var config = {name: 'scrim', classes: 'enyo-viewmanager-scrim enyo-fit'};
 			sup.apply(this, arguments);
-			this.createComponent({name: 'scrim', classes: 'enyo-viewmanager-scrim enyo-fit', isChrome: true});
-			this.on('activate', function (sender, name, event) {
-				if (this.isManager(event.view)) this.$.scrim.addClass('showing');
+
+			// override defaults with user-provided scrimConfig
+			if (this.scrimConfig) utils.mixin(config, this.scrimConfig);
+			this.createComponent(config, {isChrome: true});
+
+			// monitor the relevant events to hide and show the scrim
+			// @todo - may need to monitor active manager counts
+			this.on('manager-manage', function (sender, name, event) {
+				this.$[config.name].addClass('showing');
 			}, this);
 			this.on('manager-dismiss', function (sender, name, event) {
-				this.$.scrim.removeClass('showing');
+				this.$[config.name].removeClass('showing');
 			}, this);
 		};
 	})
