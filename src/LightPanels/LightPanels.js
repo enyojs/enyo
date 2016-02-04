993f61c2c12c7d0c9ef6fec27b36ef9ffcda724c
Aaron Tam <aaron.tam@lge.com>
2016-02-04 10:51:14 -0800

    ENYO-2913: Make return values and error conditions consistent.
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- src/LightPanels/LightPanels.js
+++ src/LightPanels/LightPanels.js
@@ -336,13 +336,15 @@ module.exports = kind(
 	* Animates to the specified panel index.
 	*
 	* @param {Number} index - The index of the panel we wish to animate a transition to.
+	* @return {Boolean} If `true`, the index to animate to was successful; `false` otherwise (i.e.
+	*	if the index is invalid, or another animation is already in progress).
 	* @public
 	*/
 	animateTo: function (index) {
 		var from = this.index;
 		this.index = index;
 		this.notifyObservers('index');
-		this.setupTransitions(from, true);
+		return this.setupTransitions(from, true);
 	},
 
 	/**
@@ -390,7 +392,8 @@ module.exports = kind(
 	* @param {Object} moreInfo - Additional properties to be applied (defaults).
 	* @param {module:enyo/LightPanels~PushPanelOptions} opts - Additional options to be used during
 	*	panel pushing.
-	* @return {Object} The instance of the panel that was created on top of the stack.
+	* @return {Object|undefined} The instance of the panel that was created; if
+	*	`undefined`, the panel could not be pushed (i.e. we are currently transitioning).
 	* @public
 	*/
 	pushPanel: function (info, moreInfo, opts) {
@@ -429,12 +432,12 @@ module.exports = kind(
 	* @param {Object} moreInfo - Additional properties to be applied (defaults).
 	* @param {module:enyo/LightPanels~PushPanelOptions} opts - Additional options to be used when
 	*	pushing multiple panels.
-	* @return {null|Object[]} Array of the panels that were created on top of the stack, or
-	*	`null` if panels could not be created.
+	* @return {Object[]|undefined} Array of the panels that were created on top of the stack; if
+	*	`undefined`, the panels could not be pushed (i.e. we are currently transitioning).
 	* @public
 	*/
 	pushPanels: function (info, moreInfo, opts) {
-		if (this.transitioning) return true;
+		if (this.transitioning) return;
 
 		if (opts && opts.purge) {
 			this.purge();
@@ -524,11 +527,16 @@ module.exports = kind(
 	* @param {Number} count - The number of panels we wish to replace.
 	* @param {Object|Object[]} info - The component definition (or array of component definitions)
 	*	for the replacement panel(s).
+	* @return {Object|Object[]|undefined} The panel or array of the panels that were pushed; if
+	*	`undefined`, the replacement could not be processed (i.e. we are currently transitioning).
 	* @public
 	*/
 	replaceAt: function (start, count, info) {
-		var panels = this.getPanels(),
-			insertBefore, commonInfo, end, idx;
+		var panels, insertBefore, commonInfo, end, idx;
+
+		if (this.transitioning) return;
+
+		panels = this.getPanels();
 
 		start = start < 0 ? panels.length + start : start;
 		end = start + count;
@@ -541,8 +549,8 @@ module.exports = kind(
 		}
 
 		// add replacement panels
-		if (utils.isArray(info)) this.pushPanels(info, commonInfo, {direct: true, force: true});
-		else this.pushPanel(info, commonInfo, {direct: true, force: true});
+		if (utils.isArray(info)) return this.pushPanels(info, commonInfo, {direct: true, force: true});
+		else return this.pushPanel(info, commonInfo, {direct: true, force: true});
 	},
 
 
@@ -721,15 +729,20 @@ module.exports = kind(
 	* @param {Number} [previousIndex] - The index of the panel we are transitioning from.
 	* @param {Boolean} [animate] - Whether or not there should be a visible animation when
 	*	transitioning between the current and next panel.
+	* @return {Boolean} If `true`, the transition was setup successfully, otherwise it was not (i.e.
+	*	we are currently transitioning or there is no "next" panel to transition to).
 	* @private
 	*/
 	setupTransitions: function (previousIndex, animate) {
-		var panels = this.getPanels(),
-			nextPanel = panels[this.index],
-			currPanel = this._currentPanel,
-			shiftCurrent, fnInitiateTransition;
+		var panels, nextPanel, currPanel, shiftCurrent, fnInitiateTransition;
 
-		if (nextPanel && !this.transitioning) {
+		if (this.transitioning) return;
+
+		panels = this.getPanels();
+		nextPanel = panels[this.index];
+		currPanel = this._currentPanel;
+
+		if (nextPanel) {
 			this.transitioning = true;
 			this._indexDirection = 0;
 
@@ -769,6 +782,8 @@ module.exports = kind(
 
 			if (!this.generated || !animate) fnInitiateTransition();
 			else animation.requestAnimationFrame(fnInitiateTransition);
+
+			return true;
 		}
 	},
 
