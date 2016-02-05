ef168b4f9092f3722e6b0d001bfde2cd1340abf3
Madala Cholan Satyanarayana <cholanmadala1510@gmail.com>
2016-02-05 12:19:18 +0530

    Documentation of Easings and Scene API's
    Enyo-DCO-1.1-Signed-off-by:Madala
    Satyanarayana<madala.satyanarayana@lge.com>

--- src/AnimationSupport/Easings.js
+++ src/AnimationSupport/Easings.js
@@ -1,24 +1,32 @@
 /**
- * Interface to achieve Easings in various animations
- *
- * @module enyo/AnimationSupport/Easings
- * @public
+ * Beginning time
+ * @type {number}
  */
-
 var b = 0,
+    /**
+     * Change in time
+     * @type {number}
+     */
     c = 1,
     temp = null,
     tempProp = null,
     tempOldState = [],
     tempNewState = [];
+/**
+ * This module provdes an interface to achieve various types of Easings in animations
+ *
+ * @module enyo/AnimationSupport/Easings
+ */
+var easings = module.exports = {
 
-var easings = {
     /**
+     * Use this function to check whether the ease object has changed or not.
      * @public
-     * apply the function to check whether the ease object has changed
-     * @params currentEase : the ease object which is currently available
+     * @param  {object} currentEase - ease object which we want to check
+     * @return {boolean}             - Boolean value for easechanged.
+     * True - Yes. The ease object got changed.
+     * False - No. The ease object has not changed.
      */
-
     easeChanged: function(currentEase) {
 
         if (temp === null) { // setting the values for the first time
@@ -35,10 +43,12 @@ var easings = {
 
         }
     },
+
     /**
+     * Use this function to check whether the animating property of the object has changed or not
      * @public
-     * apply the function to check whether the animating property of the object has changed
-     * @params currentProp : the animating property of the object which is currently available
+     * @param  {string} currentProp - Name of the animating property like - "Translate/Opacity/Scale/Rotate"
+     * @return {boolean}            - Boolean value for propChange. Either True or False
      */
     propChange: function(currentProp) {
 
@@ -56,12 +66,13 @@ var easings = {
 
         }
     },
-    /**
-     * @public
-     * apply the function to check whether the oldState of the object has changed
-     * @params currentOldState : the oldState  of the object which is currently available
-     */
 
+    /**
+     * Use this function to check whether the oldState of the object has changed or not
+     * @public
+     * @param  {object} currentOldState - currentOldState object
+     * @return {boolean}            - Boolean value for oldStateChange. Either True or False
+     */
     oldStateChange: function(currentOldState) {
 
         if (tempOldState === null) { // setting the values for the first time
@@ -79,12 +90,13 @@ var easings = {
         }
 
     },
-    /**
-     * @public
-     * apply the function to check whether the newState of the object has changed
-     * @params currentOldState : the newState  of the object which is currently available
-     */
 
+    /**
+     * Use this function to check whether the newStateChange of the object has changed or not
+     *  @public
+     * @param  {object} currentNewState -currentNewState object
+     * @return {boolean}            - Boolean value for newStateChange. Either True or False
+     */
     newStateChange: function(currentNewState) {
 
         if (tempNewState === null) { // setting the values for the first time
@@ -102,12 +114,14 @@ var easings = {
         }
 
     },
-    /**
-     * @public
-     * apply the function to compare the states which are arrays
-     * @params currentOldState : x is the previous state and y is the current state
-     */
 
+    /**
+     * Use this function to compare the states which are arrays
+     * @public
+     * @param  {Number[]} x - old array of the same 
+     * @param  {Number[]} y - current array of the same
+     * @return {boolean}   - True/ False after comparing the parameters
+     */
     compareStates: function(x, y) {
         var xLen = x.length;
         var yLen = y.length;
@@ -134,8 +148,11 @@ var easings = {
     },
 
     /**
+     * This function returns the coefficents based on the order and the current position
      * @private
-     * @params n: order, k: current position
+     * @param  {number} n - order
+     * @param  {number} k - current position
+     * @return {object}   - coefficients
      */
     getCoeff: function(n, k) {
         n = parseInt(n, 10);
@@ -157,9 +174,13 @@ var easings = {
 
         return n * this.getCoeff(n - 1, k - 1) / k;
     },
+
     /**
+     * Function to get the bezier coeffients based on the time and order
      * @public
-     * @params t: time, n: order
+     * @param  {number} t - time
+     * @param  {number} n - order
+     * @return {object}   - bezier coefficients
      */
     getBezierValues: function(t, n) {
         t = parseFloat(t, 10),
@@ -174,7 +195,7 @@ var easings = {
 
         var c,
             values = [],
-            
+
             x = (1 - t),
             y = t;
         //
@@ -188,154 +209,231 @@ var easings = {
         return values;
     },
 
-
+    /**
+     * Current time multiplied with duration
+     * @public
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number} - time 
+     */
     timeCheck: function(t, d) {
         t = t * d;
         return t;
     },
+
     /**
+     * EaseInQuad
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number} - calculated time
      */
     easeInQuad: function(t, d) {
         t = easings.timeCheck(t, d);
         return c * (t /= d) * t + b;
     },
+
     /**
+     * EaseOutQuad 
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeOutQuad: function(t, d) {
         t = easings.timeCheck(t, d);
         return -c * (t /= d) * (t - 2) + b;
     },
+
     /**
+     * EaseInOutQuad 
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInOutQuad: function(t, d) {
         t = easings.timeCheck(t, d);
         if ((t /= d / 2) < 1) return c / 2 * t * t + b;
         return -c / 2 * ((--t) * (t - 2) - 1) + b;
     },
+
     /**
+     * EaseInCubic 
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInCubic: function(t, d) {
         t = easings.timeCheck(t, d);
         return c * (t /= d) * t * t + b;
     },
+
     /**
+     * EaseOutCubic 
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeOutCubic: function(t, d) {
         t = easings.timeCheck(t, d);
         return c * ((t = t / d - 1) * t * t + 1) + b;
     },
+
     /**
+     * EaseInOutCubic
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInOutCubic: function(t, d) {
         t = easings.timeCheck(t, d);
         if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
         return c / 2 * ((t -= 2) * t * t + 2) + b;
     },
+
     /**
+     * EaseInQuart
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInQuart: function(t, d) {
         t = easings.timeCheck(t, d);
         return c * (t /= d) * t * t * t + b;
     },
+
     /**
+     * EaseOutQuart
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeOutQuart: function(t, d) {
         t = easings.timeCheck(t, d);
         return -c * ((t = t / d - 1) * t * t * t - 1) + b;
     },
+
     /**
+     * EaseInOutQuart
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInOutQuart: function(t, d) {
         t = easings.timeCheck(t, d);
         if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
         return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
     },
+
     /**
+     * EaseInQuint
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInQuint: function(t, d) {
         t = easings.timeCheck(t, d);
         return c * (t /= d) * t * t * t * t + b;
     },
     /**
+     * EaseOutQuint
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeOutQuint: function(t, d) {
         t = easings.timeCheck(t, d);
         return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
     },
+
     /**
+     * EaseInOutQuint
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInOutQuint: function(t, d) {
         t = easings.timeCheck(t, d);
         if ((t /= d / 2) < 1) return c / 2 * t * t * t * t * t + b;
         return c / 2 * ((t -= 2) * t * t * t * t + 2) + b;
     },
+
     /**
+     * EaseInSine
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInSine: function(t, d) {
         t = easings.timeCheck(t, d);
         return -c * Math.cos(t / d * (Math.PI / 2)) + c + b;
     },
+
     /**
+     * EaseOutSine
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeOutSine: function(t, d) {
         t = easings.timeCheck(t, d);
         return c * Math.sin(t / d * (Math.PI / 2)) + b;
     },
+
     /**
+     * EaseInOutSine
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInOutSine: function(t, d) {
         t = easings.timeCheck(t, d);
         return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
     },
+
     /**
+     * EaseInExpo
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInExpo: function(t, d) {
         t = easings.timeCheck(t, d);
         return (t === 0) ? b : c * Math.pow(2, 10 * (t / d - 1)) + b;
     },
+
     /**
+     * EaseOutExpo
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeOutExpo: function(t, d) {
         t = easings.timeCheck(t, d);
         return (t === d) ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b;
     },
+
     /**
+     * EaseInOutExpo
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInOutExpo: function(t, d) {
         t = easings.timeCheck(t, d);
@@ -344,34 +442,50 @@ var easings = {
         if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
         return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
     },
+
     /**
+     * EaseInCirc
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInCirc: function(t, d) {
         t = easings.timeCheck(t, d);
         return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
     },
+
     /**
+     * EaseOutCirc
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeOutCirc: function(t, d) {
         t = easings.timeCheck(t, d);
         return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
     },
+
     /**
+     * EaseInOutCirc
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInOutCirc: function(t, d) {
         t = easings.timeCheck(t, d);
         if ((t /= d / 2) < 1) return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b;
         return c / 2 * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
     },
+
     /**
+     * EaseInElastic
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInElastic: function(t, d) {
         var a = c,
@@ -387,9 +501,13 @@ var easings = {
         } else s = p / (2 * Math.PI) * Math.asin(c / a);
         return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
     },
+
     /**
+     * EaseOutElastic
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeOutElastic: function(t, d) {
         var a = c,
@@ -405,9 +523,13 @@ var easings = {
         } else s = p / (2 * Math.PI) * Math.asin(c / a);
         return a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + c + b;
     },
+
     /**
+     * EaseInOutElastic
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInOutElastic: function(t, d) {
         var a = c,
@@ -424,27 +546,39 @@ var easings = {
         if (t < 1) return -0.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
         return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * 0.5 + c + b;
     },
+
     /**
+     * EaseInBack
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInBack: function(t, d, s) {
         t = easings.timeCheck(t, d);
         if (!s) s = 1.70158;
         return c * (t /= d) * t * ((s + 1) * t - s) + b;
     },
+
     /**
+     * EaseOutBack
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeOutBack: function(t, d, s) {
         t = easings.timeCheck(t, d);
         if (s === undefined) s = 1.70158;
         return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b;
     },
+
     /**
+     * EaseInOutBack
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInOutBack: function(t, d, s) {
         t = easings.timeCheck(t, d);
@@ -452,17 +586,25 @@ var easings = {
         if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b;
         return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b;
     },
+
     /**
+     * EaseInBounce
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInBounce: function(t, d) {
         t = easings.timeCheck(t, d);
         return c - easings.easeOutBounce((d - t) / d, d) + b;
     },
+
     /**
+     * EaseOutBounce
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeOutBounce: function(t, d) {
         t = easings.timeCheck(t, d);
@@ -476,9 +618,13 @@ var easings = {
             return c * (7.5625 * (t -= (2.625 / 2.75)) * t + 0.984375) + b;
         }
     },
+
     /**
+     * EaseInOutBounce
      * @public
-     * apply the below type of ease to the DOM element 
+     * @param  {number} t - current time 
+     * @param  {number} d - duration
+     * @return {number}   calculated time
      */
     easeInOutBounce: function(t, d) {
         t = easings.timeCheck(t, d);
@@ -487,4 +633,3 @@ var easings = {
     }
 
 };
-module.exports = easings;
