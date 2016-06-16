d1cfcab9240f2b5f8784152350eebb8265ef7421
Anish_Ramesan <anish.ramesan@lge.com>
2016-06-16 21:09:14 +0530

    easing  documentation
    Enyo-DCO-1.1-Signed-off-by: Anish Ramesan <anish.ramesan@lge.com>

--- src/easing.js
+++ src/easing.js
@@ -5,40 +5,28 @@
 
 var easing = module.exports = {
 	/**
-	* linear
-	*
+	* Linear ease with no acceleration
 	* @public
 	*/
 	linear: function(n) {
 		return n;
 	},
 	/**
-	* cubicIn
-	*
+	* Accelerating with second-degree polynomial.
 	* @public
 	*/
-	cubicIn: function(n) {
-		return Math.pow(n, 3);
+	quadIn: function(t) {
+		return t * t;
 	},
 	/**
-	* cubicOut
-	*
+	* Deaccelerating with second-degree polynomial.
 	* @public
 	*/
-	cubicOut: function(n) {
-		return Math.pow(n - 1, 3) + 1;
+	quadOut: function(t) {
+		return -1 * t * (t - 2);
 	},
 	/**
-	* expoOut
-	*
-	* @public
-	*/
-	expoOut: function(n) {
-		return (n == 1) ? 1 : (-1 * Math.pow(2, -10 * n) + 1);
-	},
-	/**
-	* quadInOut
-	*
+	* Halfway accelerating and then deaccelerating with second-degree polynomial.
 	* @public
 	*/
 	quadInOut: function(n) {
@@ -48,232 +36,174 @@ var easing = module.exports = {
 		}
 		return -1 * ((--n) * (n - 2) - 1) / 2;
 	},
-
 	/**
-	 * EaseInQuad
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number} - calculated time
-	 */
-	easeInQuad: function(t) {
-		return t * t;
+	* Accelerating with third-degree polynomial.
+	* @public
+	*/
+	cubicIn: function(n) {
+		return Math.pow(n, 3);
 	},
-
 	/**
-	 * EaseOutQuad 
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeOutQuad: function(t) {
-		return -1 * t * (t - 2);
+	* Deaccelerating with third-degree polynomial.
+	* @public
+	*/
+	cubicOut: function(n) {
+		return Math.pow(n - 1, 3) + 1;
 	},
-
 	/**
-	 * EaseInOutQuad 
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInOutQuad: function(t) {
-		if ((t *= 2) < 1) return 0.5 * t * t;
-		return -0.5 * ((--t) * (t - 2) - 1);
-	},
-
-	/**
-	 * EaseInCubic 
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInCubic: function(t) {
-		return t * t * t;
-	},
-
-	/**
-	 * EaseOutCubic 
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeOutCubic: function(t) {
-		return --t * t * t + 1;
-	},
-
-	/**
-	 * EaseInOutCubic
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInOutCubic: function(t) {
+	* Halfway accelerating and then deaccelerating with third-degree polynomial.
+	* @public
+	*/
+	cubicInOut: function(t) {
 		if ((t *= 2) < 1) return 0.5 * t * t * t;
 		return 0.5 * ((t -= 2) * t * t + 2);
 	},
-
 	/**
-	 * EaseInQuart
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInQuart: function(t) {
+	* Accelerating with fourth-degree polynomial
+	* @public
+	*/
+	quartIn: function(t) {
 		return t * t * t * t;
 	},
-
 	/**
-	 * EaseOutQuart
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeOutQuart: function(t) {
+	* Deaccelerating with fourth-degree polynomial
+	* @public
+	*/
+	quartOut: function(t) {
 		return -1 * (--t * t * t * t - 1);
 	},
-
 	/**
-	 * EaseInOutQuart
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
+	* Halfway accelerating and then deaccelerating with fourth-degree polynomial
+	* @public
+	*/
 	easeInOutQuart: function(t) {
 		if ((t *= 2) < 1) return 0.5 * t * t * t * t;
 		return -0.5 * ((t -= 2) * t * t * t - 2);
 	},
-
 	/**
-	 * EaseInQuint
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInQuint: function(t) {
+	* Accelerating with fifth-degree polynomial
+	* @public
+	*/
+	quintIn: function(t) {
 		return t * t * t * t * t;
 	},
 	/**
-	 * EaseOutQuint
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeOutQuint: function(t) {
+	* Deaccelerating with fifth-degree polynomial
+	* @public
+	*/
+	quintOut: function(t) {
 		return --t * t * t * t * t + 1;
 	},
-
 	/**
-	 * EaseInOutQuint
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInOutQuint: function(t, d) {
+	* Halfway accelerating and then deaccelerating with fifth-degree polynomial
+	* @public
+	*/
+	quintInOut: function(t, d) {
 		if ((t *= 2) < 1) return 0.5 * t * t * t * t * t;
 		return 0.5 * ((t -= 2) * t * t * t * t + 2);
 	},
-
 	/**
-	 * EaseInSine
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInSine: function(t) {
+	* Accelerating using a sine formula
+	* @public
+	*/
+	sineIn: function(t) {
 		return -1 * Math.cos(t * (Math.PI / 2)) + 1;
 	},
-
 	/**
-	 * EaseOutSine
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeOutSine: function(t) {
+	* Deaccelerating using a sine formula
+	* @public
+	*/
+	sineOut: function(t) {
 		return Math.sin(t * (Math.PI / 2));
 	},
-
 	/**
-	 * EaseInOutSine
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInOutSine: function(t) {
+	* Halfway accelerating and then deaccelerating using a sine formula
+	* @public
+	*/
+	sineInOut: function(t) {
 		return -0.5 * (Math.cos(Math.PI * t) - 1);
 	},
-
 	/**
-	 * EaseInExpo
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInExpo: function(t) {
+	* Accelerating using an exponential formula
+	* @public
+	*/
+	expoIn: function(t) {
 		return (t === 0) ? 0 : Math.pow(2, 10 * (t - 1));
 	},
-
 	/**
-	 * EaseOutExpo
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeOutExpo: function(t) {
-		return (t === 1) ? 1 : -Math.pow(2, -10 * t) + 1;
+	* Deaccelerating using an exponential formula
+	* @public
+	*/
+	expoOut: function(n) {
+		return (n == 1) ? 1 : (-1 * Math.pow(2, -10 * n) + 1);
 	},
-
 	/**
-	 * EaseInOutExpo
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInOutExpo: function(t) {
+	* Halfway accelerating and then deaccelerating using an exponential formula
+	* @public
+	*/
+	expoInOut: function(t) {
 		if (t === 0) return 0;
 		if (t === 1) return 1;
 		if ((t *= 2) < 1) return 0.5 * Math.pow(2, 10 * (t - 1));
 		return 0.5 * (-Math.pow(2, -10 * --t) + 2);
 	},
-
 	/**
-	 * EaseInCirc
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInCirc: function(t) {
+	* Accelerating using a circular function
+	* @public
+	*/
+	circIn: function(t) {
 		return -1 * (Math.sqrt(1 - t * t) - 1);
 	},
-
 	/**
-	 * EaseOutCirc
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeOutCirc: function(t) {
+	* Deaccelerating using a circular function
+	* @public
+	*/
+	circOut: function(t) {
 		return Math.sqrt(1 - (--t * t));
 	},
-
 	/**
-	 * EaseInOutCirc
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInOutCirc: function(t) {
+	* Halfway accelerating and then deaccelerating using a circular function
+	* @public
+	*/
+	circInOut: function(t) {
 		if ((t *= 2) < 1) return -0.5 * (Math.sqrt(1 - t * t) - 1);
 		return 0.5 * (Math.sqrt(1 - (t -= 2) * t) + 1);
 	},
-
 	/**
-	 * EaseInElastic
-	 * @public
-	 * @param  {number} t - current time 
-	 * @param  {number} d - duration
-	 * @return {number}   calculated time
-	 */
-	easeInElastic: function(t, d) {
+	* Accelerating with a bouncing effect
+	* @public
+	*/
+	bounceIn: function(t) {
+		return 1 - easing.easeOutBounce(1 - t);
+	},
+	/**
+	* Deaccelerating with a bouncing effect
+	* @public
+	*/
+	bounceOut: function(t) {
+		if (t < 0.363636) {
+			return 7.5625 * t * t;
+		} else if (t < 0.727272) {
+			return 7.5625 * (t -= 0.545454) * t + 0.75;
+		} else if (t < (2.5 / 2.75)) {
+			return 7.5625 * (t -= 0.818182) * t + 0.9375;
+		} else {
+			return 7.5625 * (t -= 0.954545) * t + 0.984375;
+		}
+	},
+	/**
+	* Halfway accelerating and then deaccelerating with a bouncing effect
+	* @public
+	*/
+	bounceInOut: function(t) {
+		if (t < 0.5) return easing.easeInBounce(t * 2) * 0.5;
+		return easing.easeOutBounce(t * 2 - 1) * 0.5 + 0.5;
+	},
+	/**
+	* Accelerating as a spring oscillating back and forth until it comes to rest
+	* @public
+	*/
+	elasticIn: function(t, d) {
 		var a = 1,
 			p = 0,
 			s = 1.70158;
@@ -286,54 +216,11 @@ var easing = module.exports = {
 		} else s = p / (2 * Math.PI) * Math.asin(1 / a);
 		return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p));
 	},
-
 	/**
-	 * EaseInBounce
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInBounce: function(t) {
-		return 1 - easing.easeOutBounce(1 - t);
-	},
-
-	/**
-	 * EaseOutBounce
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeOutBounce: function(t) {
-		if (t < 0.363636) {
-			return 7.5625 * t * t;
-		} else if (t < 0.727272) {
-			return 7.5625 * (t -= 0.545454) * t + 0.75;
-		} else if (t < (2.5 / 2.75)) {
-			return 7.5625 * (t -= 0.818182) * t + 0.9375;
-		} else {
-			return 7.5625 * (t -= 0.954545) * t + 0.984375;
-		}
-	},
-
-	/**
-	 * EaseInOutBounce
-	 * @public
-	 * @param  {number} t - current time
-	 * @return {number}   calculated time
-	 */
-	easeInOutBounce: function(t) {
-		if (t < 0.5) return easing.easeInBounce(t * 2) * 0.5;
-		return easing.easeOutBounce(t * 2 - 1) * 0.5 + 0.5;
-	},
-
-	/**
-	 * EaseOutElastic
-	 * @public
-	 * @param  {number} t - current time 
-	 * @param  {number} d - duration
-	 * @return {number}   calculated time
-	 */
-	easeOutElastic: function(t, d) {
+	* Deaccelerating as a spring oscillating back and forth until it comes to rest
+	* @public
+	*/
+	elasticOut: function(t, d) {
 		var a = 1,
 			p = 0,
 			s = 1.70158;
@@ -346,15 +233,12 @@ var easing = module.exports = {
 		} else s = p / (2 * Math.PI) * Math.asin(1 / a);
 		return a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + 1;
 	},
-
 	/**
-	 * EaseInOutElastic
-	 * @public
-	 * @param  {number} t - current time 
-	 * @param  {number} d - duration
-	 * @return {number}   calculated time
-	 */
-	easeInOutElastic: function(t, d) {
+	* Halfway accelerating and then deaccelerating as a spring 
+	* oscillating back and forth until it comes to rest
+	* @public
+	*/
+	elasticInOut: function(t, d) {
 		var a = 1,
 			p = 0,
 			s = 1.70158;
@@ -368,39 +252,28 @@ var easing = module.exports = {
 		if (t < 1) return -0.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p));
 		return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * 0.5 + 1;
 	},
-
 	/**
-	 * EaseInBack
-	 * @public
-	 * @param  {number} t - current time 
-	 * @param  {number} d - duration
-	 * @return {number}   calculated time
-	 */
-	easeInBack: function(t, d, s) {
+	* Accelerating while retracting slightly before it begins to animate in the path indicated
+	* @public
+	*/
+	backIn: function(t, d, s) {
 		if (!s) s = 1.70158;
 		return t * t * ((s + 1) * t - s);
 	},
-
 	/**
-	 * EaseOutBack
-	 * @public
-	 * @param  {number} t - current time 
-	 * @param  {number} d - duration
-	 * @return {number}   calculated time
-	 */
-	easeOutBack: function(t, d, s) {
+	* Deaccelerating while retracting slightly before it begins to animate in the path indicated
+	* @public
+	*/
+	backOut: function(t, d, s) {
 		if (!s) s = 1.70158;
 		return --t * t * ((s + 1) * t + s) + 1;
 	},
-
 	/**
-	 * EaseInOutBack
-	 * @public
-	 * @param  {number} t - current time 
-	 * @param  {number} d - duration
-	 * @return {number}   calculated time
-	 */
-	easeInOutBack: function(t, d, s) {
+	* Halfway accelerating and then deaccelerating while retracting 
+	* slightly before it begins to animate in the path indicated
+	* @public
+	*/
+	backInOut: function(t, d, s) {
 		if (!s) s = 1.70158;
 		if ((t *= 2) < 1) return 0.5 * (t * t * (((s *= (1.525)) + 1) * t - s));
 		return 0.5 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2);
